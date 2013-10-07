9aa0c81b241ae63e29f6ac8054f3c7506cae9e84
Martin Todorov <carlspring@gmail.com>
2013-10-07 22:52:17 +0100

    Added an event for uploading checksums.

--- strongbox-event-api/src/main/java/org/carlspring/strongbox/event/ArtifactEvent.java
+++ strongbox-event-api/src/main/java/org/carlspring/strongbox/event/ArtifactEvent.java
@@ -6,17 +6,20 @@ package org.carlspring.strongbox.event;
 public class ArtifactEvent
 {
 
-    public static final int EVENT_ARTIFACT_CREATED = 1;
+    public static final int EVENT_ARTIFACT_UPLOADED = 1;
 
-    public static final int EVENT_ARTIFACT_DOWNLOADED = 2;
+    public static final int EVENT_ARTIFACT_CHECKSUM_UPLOADED = 2;
 
-    public static final int EVENT_ARTIFACT_CHANGED = 3;
+    public static final int EVENT_ARTIFACT_DOWNLOADED = 3;
 
-    public static final int EVENT_ARTIFACT_MOVED = 4;
+    public static final int EVENT_ARTIFACT_CHANGED = 4;
 
-    public static final int EVENT_ARTIFACT_ARCHIVED = 5;
+    public static final int EVENT_ARTIFACT_MOVED = 5;
 
-    public static final int EVENT_ARTIFACT_DELETED = 6;
+    public static final int EVENT_ARTIFACT_ARCHIVED = 6;
+
+    public static final int EVENT_ARTIFACT_DELETED = 7;
+
 
     private int type;
 
