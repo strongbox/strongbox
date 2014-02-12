4cb20c2356eae01c8c34442ab6b46b10f130df91
Steve Todorov <s.todorov@itnews-bg.com>
2014-02-12 03:43:34 +0200

    Added Snapshot and Release version validator classes and tests

---
+++ strongbox-storage/src/main/java/org/carlspring/strongbox/validation/SnapshotValidator.java
@@ -0,0 +1,26 @@
+package org.carlspring.strongbox.validation;
+
+import org.carlspring.strongbox.storage.repository.Repository;
+
+import org.apache.maven.artifact.Artifact;
+
+/**
+ * @author stodorov
+ */
+public class SnapshotValidator implements VersionValidator
+{
+
+    /**
+     * Matches versions:
+     * 1.0-20131004
+     * 1.0-20131004.115330
+     * 1.0-20131004.115330-1
+     */
+    @Override
+    public boolean validate(Repository repository,
+                            Artifact artifact)
+    {
+        String version = artifact.getVersion();
+        return version != null && version.matches("^([0-9]+)(\\.([0-9]+))(-(SNAPSHOT|([0-9]+)(\\.([0-9]+)(-([0-9]+))?)?))$");
+    }
+}
