d3aaaadee53e00b8c177e66f0bb0b63a4952e64e
Martin Todorov <carlspring@gmail.com>
2014-04-10 17:46:36 +0100

    #SB-150: Implement indexing for hosted repositories
    Re-worked the test case, but it's now failing.

--- strongbox-storage/src/test/java/org/carlspring/strongbox/storage/indexing/LocalRepositoryIndexerTest.java
+++ strongbox-storage/src/test/java/org/carlspring/strongbox/storage/indexing/LocalRepositoryIndexerTest.java
@@ -1,48 +1,74 @@
 package org.carlspring.strongbox.storage.indexing;
 
+import org.carlspring.maven.commons.util.ArtifactUtils;
+import org.carlspring.strongbox.artifact.generator.ArtifactGenerator;
+
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.index.ArtifactInfo;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Set;
 
 public class LocalRepositoryIndexerTest
 {
-    final File indexDir = new File("target/test-idx");
+
+    private static final File REPOSITORY_BASEDIR = new File("target/strongbox/storages/storage0/releases");
+
+    private static final File INDEX_DIR = new File(REPOSITORY_BASEDIR, ".index");
+
+    private static final Artifact artifact = ArtifactUtils.getArtifactFromGAVTC("org.carlspring.strongbox:strongbox-commons:1.0:jar");
+
 
     @Before
     public void init()
+            throws NoSuchAlgorithmException,
+                   XmlPullParserException,
+                   IOException
     {
-        indexDir.mkdirs();
+        //noinspection ResultOfMethodCallIgnored
+        INDEX_DIR.mkdirs();
+
+
+        ArtifactGenerator generator = new ArtifactGenerator(REPOSITORY_BASEDIR.getAbsolutePath(), artifact);
+        generator.generate();
     }
 
     @Test
     public void testIndex() throws Exception
     {
-        final LocalRepositoryIndexer i = new LocalRepositoryIndexer(
-                "test", new File(System.getProperty("user.home"), ".m2/repository"), indexDir);
+        final LocalRepositoryIndexer i = new LocalRepositoryIndexer("releases", REPOSITORY_BASEDIR, INDEX_DIR);
         try
         {
-            final int x = i.index(new File("antlr"));
-            Assert.assertEquals("two artifacts expected", x,
-                    2); // one is jar another pom, both would be added into the same Lucene document
+            System.out.println(REPOSITORY_BASEDIR.getAbsolutePath());
+            System.out.println(INDEX_DIR.getAbsolutePath());
 
-            Set<ArtifactInfo> search = i.search("antlr", "antlr", null);
+            final int x = i.index(REPOSITORY_BASEDIR);
+            Assert.assertEquals("Two artifacts were expected!",
+                                x,
+                                2); // One is a jar, the other -- a pom; both would be added into the same Lucene document
+
+            Set<ArtifactInfo> search = i.search(artifact.getGroupId(), artifact.getArtifactId(), null);
             for (final ArtifactInfo ai : search)
             {
                 System.out.println(ai.groupId + " / " + ai.artifactId + " / " + ai.version + " / " + ai.description);
             }
-            Assert.assertEquals("only one antlr artifact", search.size(), 1);
+
+            Assert.assertEquals("Only one " + artifact.getArtifactId() + " artifact was expected!", search.size(), 1);
 
             i.delete(search);
-            search = i.search("antlr", "antlr", null);
-            Assert.assertEquals("antlr should be deleted", search.size(), 0);
+            search = i.search(artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion());
+            Assert.assertEquals(artifact.getArtifactId() + " should have been deleted!", search.size(), 0);
         }
         finally
         {
             i.close(false);
         }
     }
+
 }
