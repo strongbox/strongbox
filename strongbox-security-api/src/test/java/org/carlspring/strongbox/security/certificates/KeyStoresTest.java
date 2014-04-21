5c69d5d9d09972a286cf3dce2e5b3515eaf1b2a7
Martin Todorov <carlspring@gmail.com>
2014-04-21 02:53:21 +0100

    #SB-179: Implement proxy support for the key managing facility
    Non-functional commit: Polished up the code a bit.

--- strongbox-security-api/src/test/java/org/carlspring/strongbox/security/certificates/KeyStoresTest.java
+++ strongbox-security-api/src/test/java/org/carlspring/strongbox/security/certificates/KeyStoresTest.java
@@ -1,8 +1,5 @@
 package org.carlspring.strongbox.security.certificates;
 
-import org.junit.Before;
-import org.junit.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -18,12 +15,28 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Map;
 
+import org.junit.Before;
+import org.junit.Test;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class KeyStoresTest
 {
+
+    private static final String PROXY_USERNAME = "testuser";
+
+    private static final String PROXY_PASSWORD = "password";
+
+    private static final Proxy PROXY_SOCKS = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("192.168.100.1", 15035));
+
+    private static final Proxy PROXY_HTTP = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.100.1", 15036));
+
+    private static final PasswordAuthentication credentials = new PasswordAuthentication(PROXY_USERNAME, PROXY_PASSWORD.toCharArray());
+
+    private static final String KEYSTORE_PASSWORD = "password";
+
     private File f;
+
 
     @Before
     public void init()
@@ -47,43 +60,51 @@ public class KeyStoresTest
     {
         KeyStores.createNew(f, "12345".toCharArray());
         //final KeyStore ks = KeyStores.addSslCertificates(f, "12345".toCharArray(), null, null, "localhost", 40636);
-        final KeyStore ks = KeyStores.addSslCertificates(f, "12345".toCharArray(),
-                new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("localhost", 9999)),
-                new PasswordAuthentication("xx", "yyy!".toCharArray()),
-                "localhost", 40636);
+        final KeyStore ks = KeyStores.addSslCertificates(f,
+                                                         KEYSTORE_PASSWORD.toCharArray(),
+                                                         PROXY_SOCKS,
+                                                         credentials,
+                                                         "localhost",
+                                                         40636);
+
         assertEquals("localhost should have one certificate in the chain", 1, ks.size());
 
         Map<String, Certificate> certs = KeyStores.listCertificates(f, "12345".toCharArray());
         for (final Map.Entry<String, Certificate> cert : certs.entrySet())
         {
-            System.out.println(cert.getKey() + " : " + ((X509Certificate)cert.getValue()).getSubjectDN());
+            System.out.println(cert.getKey() + " : " + ((X509Certificate) cert.getValue()).getSubjectDN());
         }
 
-        KeyStores.changePassword(f, "12345".toCharArray(), "666".toCharArray());
+        KeyStores.changePassword(f, KEYSTORE_PASSWORD.toCharArray(), "666".toCharArray());
         KeyStores.removeCertificates(f, "666".toCharArray(), InetAddress.getLocalHost(), 40636);
         certs = KeyStores.listCertificates(f, "666".toCharArray());
+
         assertTrue(certs.isEmpty());
     }
 
     @Test
     public void testHttp()
             throws IOException,
-            CertificateException,
-            NoSuchAlgorithmException,
-            KeyStoreException,
-            KeyManagementException
+                   CertificateException,
+                   NoSuchAlgorithmException,
+                   KeyStoreException,
+                   KeyManagementException
     {
         KeyStores.createNew(f, "12345".toCharArray());
-        final KeyStore ks = KeyStores.addHttpsCertificates(f, "12345".toCharArray(),
-                new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8888)),
-                new PasswordAuthentication("xx", "yyy!".toCharArray()),
-                "google.com", 443);
+        final KeyStore ks = KeyStores.addHttpsCertificates(f,
+                                                           KEYSTORE_PASSWORD.toCharArray(),
+                                                           PROXY_HTTP,
+                                                           credentials,
+                                                           "google.com",
+                                                           443);
+
         assertEquals("google.com should have three certificate in the chain", 3, ks.size());
 
-        Map<String, Certificate> certs = KeyStores.listCertificates(f, "12345".toCharArray());
+        Map<String, Certificate> certs = KeyStores.listCertificates(f, KEYSTORE_PASSWORD.toCharArray());
         for (final Map.Entry<String, Certificate> cert : certs.entrySet())
         {
-            System.out.println(cert.getKey() + " : " + ((X509Certificate)cert.getValue()).getSubjectDN());
+            System.out.println(cert.getKey() + " : " + ((X509Certificate) cert.getValue()).getSubjectDN());
         }
     }
+
 }
