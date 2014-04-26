d48aac223f9ac0abc960408d4511667097d99a93
Martin Todorov <carlspring@gmail.com>
2014-04-26 03:13:13 +0100

    #SB-188: Implement trash emptying
    Fixed broken test.

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/rest/TrashRestletTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/rest/TrashRestletTest.java
@@ -8,7 +8,8 @@ import java.io.File;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.handler.DefaultArtifactHandler;
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static junit.framework.Assert.assertFalse;
 
@@ -18,29 +19,35 @@ import static junit.framework.Assert.assertFalse;
 public class TrashRestletTest
 {
 
-    public static final Artifact ARTIFACT = new DefaultArtifact("org.carlspring.strongbox",
-                                                                "test-artifact-to-trash",
-                                                                "1.0",
-                                                                "compile",
-                                                                "jar",
-                                                                null,
-                                                                new DefaultArtifactHandler("jar"));
+    private static final Artifact ARTIFACT = new DefaultArtifact("org.carlspring.strongbox",
+                                                                 "test-artifact-to-trash",
+                                                                 "1.0",
+                                                                 "compile",
+                                                                 "jar",
+                                                                 null,
+                                                                 new DefaultArtifactHandler("jar"));
 
     private final static String BASEDIR = "target/strongbox/storages/storage0/releases-with-trash";
 
     private static final String STORAGE = "storage0";
 
-    public static final String REPOSITORY = "releases-with-trash";
+    private static final String REPOSITORY = "releases-with-trash";
+
+    private ArtifactClient client = new ArtifactClient();
+
+    private static final File ARTIFACT_FILE_IN_TRASH = new File("target/storages/storage0/releases-with-trash/.trash/" +
+                                                                "org/carlspring/strongbox/test-artifact-to-trash/1.0/" +
+                                                                "test-artifact-to-trash-1.0.jar").getAbsoluteFile();
 
 
-    @Test
-    public void testDeleteArtifactAndEmptyTrashForRepository()
+    @Before
+    public void setUp()
             throws Exception
     {
         ArtifactGenerator generator = new ArtifactGenerator(BASEDIR);
         generator.generate(ARTIFACT);
 
-        ArtifactClient client = new ArtifactClient();
+        client = new ArtifactClient();
         client.setUsername("maven");
         client.setPassword("password");
         client.setPort(48080);
@@ -48,39 +55,32 @@ public class TrashRestletTest
 
         // Delete the artifact
         client.deleteArtifact(ARTIFACT, STORAGE, REPOSITORY);
+    }
+
+    @After
+    public void tearDown()
+            throws Exception
+    {
+        //noinspection ResultOfMethodCallIgnored
+        ARTIFACT_FILE_IN_TRASH.delete();
+    }
+
+    @Test
+    public void testDeleteArtifactAndEmptyTrashForRepository()
+            throws Exception
+    {
         client.deleteTrash(REPOSITORY);
 
-        File artifactFileInTrash = new File("target/storages/storage0/releases-with-trash/.trash/" +
-                                            "org/carlspring/strongbox/test-artifact-to-trash/1.0/" +
-                                            "test-artifact-to-trash-1.0.jar").getAbsoluteFile();
-
-
-        assertFalse("Failed to empty trash for repository '" + REPOSITORY + "'!", artifactFileInTrash.exists());
+        assertFalse("Failed to empty trash for repository '" + REPOSITORY + "'!", ARTIFACT_FILE_IN_TRASH.exists());
     }
 
     @Test
     public void testDeleteArtifactAndEmptyTrashForAllRepositories()
             throws Exception
     {
-        ArtifactGenerator generator = new ArtifactGenerator(BASEDIR);
-        generator.generate(ARTIFACT);
-
-        ArtifactClient client = new ArtifactClient();
-        client.setUsername("maven");
-        client.setPassword("password");
-        client.setPort(48080);
-        client.setContextBaseUrl("trash");
-
-        // Delete the artifact
-        client.deleteArtifact(ARTIFACT, STORAGE, REPOSITORY);
         client.deleteTrash();
 
-        File artifactFileInTrash = new File("target/storages/storage0/releases-with-trash/.trash/" +
-                                            "org/carlspring/strongbox/test-artifact-to-trash/1.0/" +
-                                            "test-artifact-to-trash-1.0.jar").getAbsoluteFile();
-
-
-        assertFalse("Failed to empty trash for repository '" + REPOSITORY + "'!", artifactFileInTrash.exists());
+        assertFalse("Failed to empty trash for repository '" + REPOSITORY + "'!", ARTIFACT_FILE_IN_TRASH.exists());
     }
 
 }
