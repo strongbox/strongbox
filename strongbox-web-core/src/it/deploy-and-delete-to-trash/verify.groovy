c226383bedb779558166a681cff21a659182488f
Martin Todorov <carlspring@gmail.com>
2014-04-30 18:52:54 +0100

    #SB-190: Remove checksums for artifacts when they have been removed via the REST API
    Adjusted the test case to check for the checksums.

--- strongbox-web-core/src/it/deploy-and-delete-to-trash/verify.groovy
+++ strongbox-web-core/src/it/deploy-and-delete-to-trash/verify.groovy
@@ -17,5 +17,13 @@ def artifactFileInTrash = new File(System.getProperty("strongbox.storage.booter.
                                    "/storage0/releases-with-trash/.trash/" +
                                    "org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar").getAbsoluteFile();
 
+def artifactMd5FileInTrash = new File(System.getProperty("strongbox.storage.booter.basedir") +
+                                      "/storage0/releases-with-trash/.trash/" +
+                                      "org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar.md5").getAbsoluteFile();
+
+def artifactSha1FileInTrash = new File(System.getProperty("strongbox.storage.booter.basedir") +
+                                       "/storage0/releases-with-trash/.trash/" +
+                                       "org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar.sha1").getAbsoluteFile();
+
 return !client.artifactExists(artifact, "storage0", "releases-with-trash") && !artifactFile.exists() &&
-       artifactFileInTrash.exists();
+       artifactFileInTrash.exists() && artifactMd5FileInTrash.exists() && artifactSha1FileInTrash.exists();
