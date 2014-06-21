6315bb88ccbbd183f5b0b34bef7db2de9f2f4c09
Martin Todorov <carlspring@gmail.com>
2014-06-21 13:58:02 +0100

    #SB-203: Implement artifact searching with results in plain text
    Made the test not depend on artifacts produced by other tests.

--- strongbox-web-core/src/it/search-plain-text/verify.groovy
+++ strongbox-web-core/src/it/search-plain-text/verify.groovy
@@ -8,8 +8,8 @@ client.setPassword("password");
 def r0 = client.searchLucene("releases", "g:org.carlspring.maven v:1.0.11 p:pom");
 def r1 = client.searchLucene("releases", "g:org.carlspring.maven v:1.0.11 p:p*");
 def r2 = client.searchLucene("releases", "g:org.carlspring.*");
-def r3 = client.searchLucene("releases", "g:org.carlspring.* v:1.0.5");
-def r4 = client.searchLucene("releases", "u:org.carlspring.*|1.0*");
+def r3 = client.searchLucene("releases", "g:org.carlspring.* v:1.0.1*   ");
+def r4 = client.searchLucene("releases", "u:org.carlspring.*|1.0.12*");
 def r5 = client.searchLucene("u:org.carlspring.*");
 
 System.out.println("*** g:org.carlspring.maven v:1.0.11 p:pom ***");
@@ -30,4 +30,4 @@ System.out.println("*********************************************");
 
 return r0.indexOf("1.0.11") >= 0 && r1.indexOf("1.0.11") >= 0 &&
        r2.indexOf("1.0.11") >= 0 && r3.indexOf("1.0.5") < 0 &&
-       r4.indexOf("1.0.10") >= 0 && r5.contains("1.0.10");
+       r4.indexOf("1.0.12") >= 0 && r5.contains("1.0.12");
