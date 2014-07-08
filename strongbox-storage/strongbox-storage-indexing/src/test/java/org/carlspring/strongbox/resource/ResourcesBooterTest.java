1276c23298114fe2ba3edfdef5be8ea90405d272
Martin Todorov <carlspring@gmail.com>
2014-07-08 02:20:17 +0100

    SB-212: Implement a class that locates all configuration files on the classpath and copies them to etc/config
    Implemented.

---
+++ strongbox-storage/strongbox-storage-indexing/src/test/java/org/carlspring/strongbox/resource/ResourcesBooterTest.java
@@ -0,0 +1,42 @@
+package org.carlspring.strongbox.resource;
+
+import org.carlspring.strongbox.booters.ResourcesBooter;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author mtodorov
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/META-INF/spring/strongbox-*-context.xml", "classpath*:/META-INF/spring/strongbox-*-context.xml"})
+public class ResourcesBooterTest
+{
+
+    static
+    {
+        System.setProperty("strongbox.basedir", "target/strongbox");
+    }
+
+    // This field is indeed used. It's execute() method is being invoked with a @PostConstruct.
+    @SuppressWarnings("UnusedDeclaration")
+    @Autowired
+    private ResourcesBooter resourcesBooter;
+
+
+    @Test
+    public void testResourceBooting()
+            throws Exception
+    {
+        File file = new File("target/strongbox/etc/conf/simple-test.xml");
+
+        assertTrue("Failed to copy configuration resource from classpath!", file.exists());
+    }
+
+}
