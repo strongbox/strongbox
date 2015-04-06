bd3000cb3e1d9392db367e50f46fabbc4e24b484
Martin Todorov <carlspring@gmail.com>
2015-04-06 04:59:55 +0100

    Sonar recommended fix: Useless imports should be removed.

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/storage/repository/RemoteRepository.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/storage/repository/RemoteRepository.java
@@ -1,8 +1,9 @@
 package org.carlspring.strongbox.storage.repository;
 
-import org.carlspring.strongbox.security.jaas.Credentials;
-
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * @author mtodorov
