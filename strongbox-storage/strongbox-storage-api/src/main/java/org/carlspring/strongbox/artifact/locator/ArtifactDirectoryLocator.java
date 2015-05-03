0e1c96d4f15deb10ffb587a4239e74094a73f321
Martin Todorov <carlspring@gmail.com>
2015-05-03 02:19:50 +0100

    SB-244: Create a more sophisticated version directory scanner
    Implemented.

---
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/artifact/locator/ArtifactDirectoryLocator.java
@@ -0,0 +1,83 @@
+package org.carlspring.strongbox.artifact.locator;
+
+import org.carlspring.strongbox.artifact.locator.handlers.ArtifactDirectoryOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * @author mtodorov
+ */
+public class ArtifactDirectoryLocator
+{
+
+    private static final Logger logger = LoggerFactory.getLogger(ArtifactDirectoryLocator.class);
+
+    private ArtifactDirectoryOperation operation;
+
+    /**
+     * The basedir to start analyzing from. Define this only,
+     * if you need to pass null values for operation.storage and operation.repository.
+     */
+    private String basedir;
+
+
+    public void locateArtifactDirectories()
+            throws IOException
+    {
+        long startTime = System.currentTimeMillis();
+
+        Files.walk(Paths.get(getStartingPath()))
+             .filter(Files::isDirectory)
+             // Skip directories which start with a dot (like, for example: .index)
+             .filter(path -> !path.toAbsolutePath().toString().matches(".*/\\..*"))
+             // Note: Sorting can be expensive:
+             .sorted()
+             .forEach(operation::execute);
+
+        long endTime = System.currentTimeMillis();
+
+        logger.debug("Executed (cache: " + operation.getVisitedRootPaths().size() + ")" +
+                     " visits in " + (endTime - startTime) + " ms.");
+    }
+
+    public String getStartingPath()
+    {
+        // The root path
+        String rootPath = basedir != null ? basedir : getOperation().getRepository().getBasedir();
+
+        // The base path to be appended to the root path
+        String basePath = getOperation().getBasePath() != null ? getOperation().getBasePath() : "";
+        if (!StringUtils.isEmpty(basePath) && !basePath.startsWith("/"))
+        {
+            basePath = "/" + basePath;
+        }
+
+        return rootPath + basePath;
+    }
+
+    public ArtifactDirectoryOperation getOperation()
+    {
+        return operation;
+    }
+
+    public void setOperation(ArtifactDirectoryOperation operation)
+    {
+        this.operation = operation;
+    }
+
+    public String getBasedir()
+    {
+        return basedir;
+    }
+
+    public void setBasedir(String basedir)
+    {
+        this.basedir = basedir;
+    }
+
+}
