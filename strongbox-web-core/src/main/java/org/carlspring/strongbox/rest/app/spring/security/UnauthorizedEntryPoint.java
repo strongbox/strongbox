548cbcc0c1c532a8b8bc27adb7e9d5423a0edd26
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2016-03-16 00:03:04 +0100

    SB-420 Docu fixes

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/rest/app/spring/security/UnauthorizedEntryPoint.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/rest/app/spring/security/UnauthorizedEntryPoint.java
@@ -16,7 +16,7 @@ public class UnauthorizedEntryPoint implements AuthenticationEntryPoint {
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
 			throws IOException, ServletException {
 
-		// Use the type of authException to decide what to present to the use.
+		// Use the type of authException to decide what to present to the user.
 		// This simple impl just redirects to /login but it could be that the
 		// user is already logged in and (s)he has not enough rights
 		response.sendRedirect("/login");
