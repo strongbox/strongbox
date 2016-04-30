0d5092c36783701b24e40232e0e74a19fa33e60b
Ivan Ursul <Ivanon2@gmail.com>
2016-04-30 16:27:16 +0300

    SB-520: Migrate Spring contexts and web.xml to Java configurations:Removed strongbox-maven-indexer-context.xml

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/config/WebConfig.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/config/WebConfig.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-/**
- * @since 4/30/16
- */
 @Configuration
 @ComponentScan("org.carlspring.strongbox")
 @Import({
         CommonConfig.class,
-        SecurityConfig.class
+        SecurityConfig.class,
+        StorageIndexingConfig.class
 })
 public class WebConfig {
 }
