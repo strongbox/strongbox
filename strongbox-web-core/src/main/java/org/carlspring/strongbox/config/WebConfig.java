b2da95440320d6fcf0436da187a3b5cedcc1ceca
Ivan Ursul <Ivanon2@gmail.com>
2016-04-30 14:55:03 +0300

    Added WebConfig

---
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/config/WebConfig.java
@@ -0,0 +1,16 @@
+package org.carlspring.strongbox.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @since 4/30/16
+ */
+@Configuration
+@ComponentScan("org.carlspring.strongbox")
+@Import({
+
+})
+public class WebConfig {
+}
