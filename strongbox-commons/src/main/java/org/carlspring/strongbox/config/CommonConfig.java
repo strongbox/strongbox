9e6970bc32619cf4318ce5b39b023bfe0e44ec6c
Ivan Ursul <Ivanon2@gmail.com>
2016-05-04 11:12:03 +0300

    added separate package per module

--- strongbox-commons/src/main/java/org/carlspring/strongbox/config/CommonConfig.java
+++ strongbox-commons/src/main/java/org/carlspring/strongbox/config/CommonConfig.java
@@ -2,13 +2,12 @@ package org.carlspring.strongbox.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("org.carlspring.strongbox.common")
-public class CommonConfig extends AnnotationConfigApplicationContext
+public class CommonConfig
 {
     private static final Logger logger = LoggerFactory.getLogger(CommonConfig.class);
 
