8ed5bef785c8b481fb4687f7d32288645e1807a7
alex.oreshkevich <alex.oreshkevich@redsoft.pro>
2016-06-03 12:46:36 +0300

    SB-542 Implement embedded OrientDB.

--- strongbox-data-service/src/main/java/org/carlspring/strongbox/data/config/DataServiceConfig.java
+++ strongbox-data-service/src/main/java/org/carlspring/strongbox/data/config/DataServiceConfig.java
@@ -1,14 +1,17 @@
 package org.carlspring.strongbox.data.config;
 
 import org.carlspring.strongbox.data.domain.StrongboxUser;
+import org.carlspring.strongbox.data.server.EmbeddedOrientDbServer;
 import org.carlspring.strongbox.data.tx.CustomOrientTransactionManager;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.IOException;
 
 import com.orientechnologies.orient.client.remote.OServerAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -52,6 +55,9 @@ public class DataServiceConfig
     @Value("${org.carlspring.strongbox.data.orientdb.password}")
     String password;
 
+    @Autowired
+    EmbeddedOrientDbServer embeddableServer;
+
     @Bean
     public OrientObjectDatabaseFactory factory()
     {
@@ -79,8 +85,10 @@ public class DataServiceConfig
             throws IOException
     {
 
+        embeddableServer.start();
+
         // create database if not initialized
-        OServerAdmin serverAdmin = new OServerAdmin(getConnectionUrl()).connect("root", "root");
+        OServerAdmin serverAdmin = new OServerAdmin(getConnectionUrl()).connect(user, password);
         if (!serverAdmin.existsDatabase())
         {
             logger.debug("Create database " + database);
@@ -89,6 +97,11 @@ public class DataServiceConfig
 
         // register all domain entities
         factory().db().getEntityManager().registerEntityClasses(DOMAIN_PACKAGE);
+    }
+
+    @PreDestroy
+    public void shutDown(){
+        embeddableServer.shutDown();
     }
 
     private String getConnectionUrl()
