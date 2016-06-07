0961acc17b18e6123f1102646da52c50c83104fe
alex.oreshkevich <alex.oreshkevich@redsoft.pro>
2016-06-07 10:09:03 +0300

    manual commit of testing config

---
+++ strongbox-testing/strongbox-testing-core/src/main/java/org/carlspring/strongbox/users/data/config/TestingCoreConfig.java
@@ -0,0 +1,20 @@
+package org.carlspring.strongbox.users.data.config;
+
+import org.carlspring.strongbox.testing.AssignedPorts;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * TestingCoreConfig.
+ */
+@Configuration
+public class TestingCoreConfig
+{
+
+    @Bean(name = "assignedPorts")
+    AssignedPorts assignedPorts()
+    {
+        return new AssignedPorts();
+    }
+
+}
