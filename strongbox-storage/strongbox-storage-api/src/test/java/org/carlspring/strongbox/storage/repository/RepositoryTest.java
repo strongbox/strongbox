211b61c0af04d29c3ff5d33c1801268b68827483
carlspring <carlspring@gmail.com>
2016-11-21 19:16:23 +0000

    SB-710: Make it possible to specify custom configuration for repositories
    Okay... spoke too soon.

--- strongbox-storage/strongbox-storage-api/src/test/java/org/carlspring/strongbox/storage/repository/RepositoryTest.java
+++ strongbox-storage/strongbox-storage-api/src/test/java/org/carlspring/strongbox/storage/repository/RepositoryTest.java
@@ -1,5 +1,10 @@
 package org.carlspring.strongbox.storage.repository;
 
+import org.carlspring.strongbox.config.ClientConfig;
+import org.carlspring.strongbox.config.CommonConfig;
+import org.carlspring.strongbox.config.DataServiceConfig;
+import org.carlspring.strongbox.config.StorageApiConfig;
+import org.carlspring.strongbox.configuration.ConfigurationManager;
 import org.carlspring.strongbox.storage.Storage;
 import org.carlspring.strongbox.xml.parsers.GenericParser;
 
@@ -8,20 +13,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static org.junit.Assert.assertTrue;
 
 /**
  * @author carlspring
  */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration
 public class RepositoryTest
 {
+
+/*
+    @org.springframework.context.annotation.Configuration
+    @Import({ StorageApiConfig.class,
+              CommonConfig.class,
+              ClientConfig.class,
+              DataServiceConfig.class
+            })
+    public static class SpringConfig { }
+
+    @Autowired
+    private ConfigurationManager configurationManager;
+*/
 
 
     @Test
     public void testRepositoryWithCustomConfiguration()
             throws JAXBException
     {
-        Storage storage = new Storage("aws-test-storage");
+        Repository repository = createTestRepository();
+
+        GenericParser<Repository> parser = new GenericParser<>(Repository.class,
+                                                               AwsConfiguration.class,
+                                                               GoogleCloudConfiguration.class);
+        String serialized = parser.serialize(repository);
+
+        System.out.println(serialized);
+
+        assertTrue(serialized.contains("<aws-configuration bucket=\"test-bucket\" key=\"test-key\"/>"));
+        assertTrue(serialized.contains("<google-cloud-configuration bucket=\"test-bucket\" key=\"test-key\"/>"));
+    }
+
+    @Test
+    public void testAddRepositoryWithCustomConfiguration()
+    {
+        Repository repository = createTestRepository();
+
+        // configurationManager.getConfiguration().getStorage("storage0").addOrUpdateRepository(repository);
+    }
+
+    private Repository createTestRepository()
+    {
+        Storage storage = new Storage("storage0");
         Repository repository = new Repository("test-repository");
         repository.setStorage(storage);
 
@@ -39,15 +87,7 @@ public class RepositoryTest
 
         repository.setCustomConfigurations(customConfigurations);
 
-        GenericParser<Repository> parser = new GenericParser<>(Repository.class,
-                                                               AwsConfiguration.class,
-                                                               GoogleCloudConfiguration.class);
-        String serialized = parser.serialize(repository);
-
-        System.out.println(serialized);
-
-        assertTrue(serialized.contains("<aws-configuration bucket=\"test-bucket\" key=\"test-key\"/>"));
-        assertTrue(serialized.contains("<google-cloud-configuration bucket=\"test-bucket\" key=\"test-key\"/>"));
+        return repository;
     }
 
 }
