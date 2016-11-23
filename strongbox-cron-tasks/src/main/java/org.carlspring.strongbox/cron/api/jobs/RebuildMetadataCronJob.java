981b0a283c76e5d57db5600f5daf3d42d864bfac
Kate Novik <ekat.novik@gmail.com>
2016-11-23 17:02:41 +0300

    SB-564 : Add code fixes by pul request comments (#184).

--- strongbox-cron-tasks/src/main/java/org.carlspring.strongbox/cron/api/jobs/RebuildMetadataCronJob.java
+++ strongbox-cron-tasks/src/main/java/org.carlspring.strongbox/cron/api/jobs/RebuildMetadataCronJob.java
@@ -1,24 +1,22 @@
 package org.carlspring.strongbox.cron.api.jobs;
 
-import org.carlspring.strongbox.cron.config.ApplicationContextProvider;
+import org.carlspring.strongbox.configuration.ConfigurationManager;
 import org.carlspring.strongbox.cron.config.JobManager;
 import org.carlspring.strongbox.cron.domain.CronTaskConfiguration;
-import org.carlspring.strongbox.resource.ConfigurationResourceResolver;
-import org.carlspring.strongbox.services.impl.ArtifactMetadataServiceImpl;
+import org.carlspring.strongbox.services.ArtifactMetadataService;
+import org.carlspring.strongbox.storage.Storage;
+import org.carlspring.strongbox.storage.repository.Repository;
 
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 /**
  * @author Kate Novik
@@ -27,15 +25,17 @@ public class RebuildMetadataCronJob
         extends JavaCronJob
 {
 
-    private static final String BASEDIR_STORAGES = "storages";
-
-    private static final String storagesBasePath = getStoragesBasePath();
-
     private final Logger logger = LoggerFactory.getLogger(RebuildMetadataCronJob.class);
 
-    private ArtifactMetadataServiceImpl artifactMetadataService = getArtifactMetadataService();
+    @Autowired
+    private ArtifactMetadataService artifactMetadataService;
 
-    private JobManager manager = getJobManager();
+    @Autowired
+    private ConfigurationManager configurationManager;
+
+    @Autowired
+    private JobManager manager;
+
 
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext)
@@ -52,19 +52,15 @@ public class RebuildMetadataCronJob
 
             if (storageId == null)
             {
-                List<String> storagesId = getStoragesId();
-                for (String storage : storagesId)
+                Map<String, Storage> storages = getStorages();
+                for (String storage : storages.keySet())
                 {
-                    Path storagePath = Paths.get(getStoragesBasePath(), storage);
-                    List<String> repositoriesId = getRepositoriesId(storagePath);
-                    rebuildRepositories(repositoriesId, storage, basePath);
+                    rebuildRepositories(storage);
                 }
             }
             else if (repositoryId == null)
             {
-                Path storagePath = Paths.get(getStoragesBasePath(), storageId);
-                List<String> repositoriesId = getRepositoriesId(storagePath);
-                rebuildRepositories(repositoriesId, storageId, basePath);
+                rebuildRepositories(storageId);
             }
             else
             {
@@ -73,99 +69,39 @@ public class RebuildMetadataCronJob
         }
         catch (IOException | XmlPullParserException | NoSuchAlgorithmException e)
         {
-            e.printStackTrace();
-            logger.error("IOException: ", e);
+            logger.error(e.getMessage(), e);
         }
+
         manager.addExecutedJob(config.getName(), true);
     }
 
     /**
      * To rebuild artifact's metadata in repositories
      *
-     * @param repositories list of repositories
-     * @param storageId    path of storage
-     * @param basePath     basePath of artifact
+     * @param storageId path of storage
      * @throws NoSuchAlgorithmException
      * @throws XmlPullParserException
      * @throws IOException
      */
-    private void rebuildRepositories(List<String> repositories,
-                                     String storageId,
-                                     String basePath)
+    private void rebuildRepositories(String storageId)
             throws NoSuchAlgorithmException, XmlPullParserException, IOException
     {
-        for (String repository : repositories)
+        Map<String, Repository> repositories = getRepositories(storageId);
+
+        for (String repository : repositories.keySet())
         {
-            artifactMetadataService.rebuildMetadata(storageId, repository, basePath);
+            artifactMetadataService.rebuildMetadata(storageId, repository, null);
         }
     }
 
-    /**
-     * To get list of repositoryId in storage
-     *
-     * @param storage
-     * @return list of repositoryId
-     */
-    private List<String> getRepositoriesId(Path storage)
+    private Map<String, Storage> getStorages()
     {
-        File file = storage.toFile();
-        return getListDirectories(file);
+        return configurationManager.getConfiguration().getStorages();
     }
 
-    /**
-     * To get list of storageId
-     *
-     * @return list of storageId
-     */
-    private List<String> getStoragesId()
+    private Map<String, Repository> getRepositories(String storageId)
     {
-        File file = Paths.get(storagesBasePath).toFile();
-        return getListDirectories(file);
-    }
-
-    /**
-     * To get list of directories
-     *
-     * @param baseFile Object File
-     * @return list of directories
-     */
-    private List<String> getListDirectories(File baseFile)
-    {
-        List<String> listDirectories = new ArrayList<>();
-        File[] listFiles = baseFile.listFiles();
-
-        if (listFiles != null)
-        {
-            for (File file : listFiles)
-            {
-                if (file.isDirectory())
-                {
-                    String directoryId = file.getAbsolutePath().substring(baseFile.getAbsolutePath().length() + 1);
-                    listDirectories.add(directoryId);
-                }
-            }
-        }
-        return listDirectories;
-    }
-
-    /**
-     * To get base path of storages
-     *
-     * @return String base path
-     */
-    private static String getStoragesBasePath()
-    {
-        return Paths.get(ConfigurationResourceResolver.getVaultDirectory(), BASEDIR_STORAGES).toString();
-    }
-
-    private ArtifactMetadataServiceImpl getArtifactMetadataService()
-    {
-        return ApplicationContextProvider.getApplicationContext().getBean(ArtifactMetadataServiceImpl.class);
-    }
-
-    private JobManager getJobManager()
-    {
-        return ApplicationContextProvider.getApplicationContext().getBean(JobManager.class);
+        return getStorages().get(storageId).getRepositories();
     }
 
 }
