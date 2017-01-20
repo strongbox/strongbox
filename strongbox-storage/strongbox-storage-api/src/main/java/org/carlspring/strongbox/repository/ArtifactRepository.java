865b6040134f4b1a0010e10cc0ccd76757fcda44
alex.oreshkevich <alex.oreshkevich@redsoft.pro>
2017-01-20 16:43:51 +0300

    SB-711 Code cleanup

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/repository/ArtifactRepository.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/repository/ArtifactRepository.java
@@ -3,9 +3,6 @@ package org.carlspring.strongbox.repository;
 import org.carlspring.strongbox.data.repository.OrientRepository;
 import org.carlspring.strongbox.domain.ArtifactEntry;
 
-import java.util.List;
-
-import org.springframework.data.orient.commons.repository.annotation.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -18,14 +15,5 @@ public interface ArtifactRepository
         extends OrientRepository<ArtifactEntry>
 {
 
-    @Query("select * from ArtifactEntry where artifactCoordinates.artifactId = ?")
-    List<ArtifactEntry> findByArtifactId(String artifactId);
-
-    @Query("select * from ArtifactEntry where artifactCoordinates.groupId = ?")
-    List<ArtifactEntry> findByGroupId(String groupId);
-
-    @Query("select * from ArtifactEntry where artifactCoordinates.artifactId = ? and artifactCoordinates.groupId = ?")
-    List<ArtifactEntry> findByArtifactIdAndGroupId(String artifactId,
-                                                   String groupId);
 }
 
