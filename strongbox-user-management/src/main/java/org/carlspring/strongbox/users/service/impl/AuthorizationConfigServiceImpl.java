4a9012116ba91dfe0d16821a5c49c919ca2f0058
alex.oreshkevich <alex.oreshkevich@redsoft.pro>
2017-01-30 21:01:31 +0300

    SB-733 Trying to resolve Inconsistent synchronization issue reported by SonarQube

--- strongbox-user-management/src/main/java/org/carlspring/strongbox/users/service/impl/AuthorizationConfigServiceImpl.java
+++ strongbox-user-management/src/main/java/org/carlspring/strongbox/users/service/impl/AuthorizationConfigServiceImpl.java
@@ -27,67 +27,67 @@ class AuthorizationConfigServiceImpl
     AuthorizationConfigRepository repository;
 
     @Override
-    public synchronized <S extends AuthorizationConfig> S save(S var1)
+    public <S extends AuthorizationConfig> S save(S var1)
     {
         return repository.save(var1);
     }
 
     @Override
-    public synchronized <S extends AuthorizationConfig> Iterable<S> save(Iterable<S> var1)
+    public <S extends AuthorizationConfig> Iterable<S> save(Iterable<S> var1)
     {
         return repository.save(var1);
     }
 
     @Override
-    public synchronized Optional<AuthorizationConfig> findOne(String var1)
+    public Optional<AuthorizationConfig> findOne(String var1)
     {
         return Optional.ofNullable(repository.findOne(var1));
     }
 
     @Override
-    public synchronized boolean exists(String var1)
+    public boolean exists(String var1)
     {
         return repository.exists(var1);
     }
 
     @Override
-    public synchronized Optional<List<AuthorizationConfig>> findAll()
+    public Optional<List<AuthorizationConfig>> findAll()
     {
         return Optional.ofNullable(repository.findAll());
     }
 
     @Override
-    public synchronized Optional<List<AuthorizationConfig>> findAll(List<String> var1)
+    public Optional<List<AuthorizationConfig>> findAll(List<String> var1)
     {
         return Optional.ofNullable(repository.findAll(var1));
     }
 
     @Override
-    public synchronized long count()
+    public long count()
     {
         return repository.count();
     }
 
     @Override
-    public synchronized void delete(String var1)
+    public void delete(String var1)
     {
         repository.delete(var1);
     }
 
     @Override
-    public synchronized void delete(AuthorizationConfig var1)
+    public void delete(AuthorizationConfig var1)
     {
         repository.delete(var1);
     }
 
     @Override
-    public synchronized void delete(Iterable<? extends AuthorizationConfig> var1)
+    public void delete(Iterable<? extends AuthorizationConfig> var1)
     {
         repository.delete(var1);
     }
 
     @Override
-    public synchronized void deleteAll()
+    public void deleteAll()
     {
         repository.deleteAll();
     }
