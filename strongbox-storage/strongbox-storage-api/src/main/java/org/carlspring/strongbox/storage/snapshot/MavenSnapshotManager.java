7957beefd04265cf8694b521bc7c5a8033c89f59
Kate Novik <ekat.novik@gmail.com>
2017-02-20 12:20:46 +0300

    SB-734: Add functions for definition removing timestamped snapshots.

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/storage/snapshot/MavenSnapshotManager.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/storage/snapshot/MavenSnapshotManager.java
@@ -6,8 +6,6 @@ import org.carlspring.strongbox.providers.layout.LayoutProvider;
 import org.carlspring.strongbox.providers.layout.LayoutProviderRegistry;
 import org.carlspring.strongbox.providers.storage.StorageProviderRegistry;
 import org.carlspring.strongbox.storage.metadata.MavenMetadataManager;
-import org.carlspring.strongbox.storage.metadata.MetadataHelper;
-import org.carlspring.strongbox.storage.metadata.MetadataType;
 import org.carlspring.strongbox.storage.metadata.VersionCollectionRequest;
 import org.carlspring.strongbox.storage.repository.Repository;
 
@@ -18,15 +16,19 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.repository.metadata.Metadata;
-import org.apache.maven.artifact.repository.metadata.Snapshot;
 import org.apache.maven.artifact.repository.metadata.Versioning;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.slf4j.Logger;
@@ -40,6 +42,8 @@ import static org.carlspring.strongbox.providers.layout.LayoutProviderRegistry.g
 @Component
 public class MavenSnapshotManager
 {
+
+    private static final String TIMESTAMP_FORMAT = "yyyyMMdd.HHmmss";
 
     private static final Logger logger = LoggerFactory.getLogger(MavenSnapshotManager.class);
 
@@ -61,108 +65,185 @@ public class MavenSnapshotManager
                                                    VersionCollectionRequest request,
                                                    int numberToKeep,
                                                    int keepPeriod)
-            throws IOException, ProviderImplementationException, NoSuchAlgorithmException
+            throws IOException, ProviderImplementationException, NoSuchAlgorithmException, ParseException, XmlPullParserException
     {
-//        LayoutProvider layoutProvider = getLayoutProvider(repository, layoutProviderRegistry);
-//        if (layoutProvider.containsPath(repository, artifactPath))
-//        {
-//            logger.debug("Removal of timestamped Maven snapshot artifact " + artifactPath +
-//                         " in '" + repository.getStorage()
-//                                             .getId() + ":" + repository.getId() + "'.");
-//            Versioning versioning = request.getVersioning();
-//            Artifact artifact = ArtifactUtils.convertPathToArtifact(artifactPath);
-//
-//            if (!versioning.getVersions()
-//                           .isEmpty())
-//            {
-//                for (String version : versioning.getVersions())
-//                {
-//
-//                    Path versionBasePath = Paths.get(request.getArtifactBasePath()
-//                                                            .toString(),
-//                                                     ArtifactUtils.getSnapshotBaseVersion(version));
-//
-//                    removeTimestampedSnapshot(versionBasePath.toString(), repository, numberToKeep, keepPeriod);
-//
-//                    mavenMetadataManager.generateSnapshotVersioningMetadata(versionBasePath, artifact, version, true);
-//
-//                    logger.debug("Generated Maven checksum for " + versionBasePath + ".");
-//                }
-//            }
-//
-//        }
-//        else
-//        {
-//            logger.error("Removal of timestamped Maven snapshot artifact: " + artifactPath + ".");
-//        }
+        LayoutProvider layoutProvider = getLayoutProvider(repository, layoutProviderRegistry);
+        if (layoutProvider.containsPath(repository, artifactPath))
+        {
+            logger.debug("Removal of timestamped Maven snapshot artifact " + artifactPath +
+                         " in '" + repository.getStorage()
+                                             .getId() + ":" + repository.getId() + "'.");
+            Versioning versioning = request.getVersioning();
+            Artifact artifact = ArtifactUtils.convertPathToArtifact(artifactPath);
+
+            if (!versioning.getVersions()
+                           .isEmpty())
+            {
+                for (String version : versioning.getVersions())
+                {
+
+                    Path versionBasePath = Paths.get(request.getArtifactBasePath()
+                                                            .toString(),
+                                                     ArtifactUtils.getSnapshotBaseVersion(version));
+
+                    if (removeTimestampedSnapshot(versionBasePath.toString(), repository, numberToKeep, keepPeriod))
+                    {
+
+                        logger.debug("Generate snapshot versioning metadata for " + versionBasePath + ".");
+
+                        mavenMetadataManager.generateSnapshotVersioningMetadata(versionBasePath, artifact, version,
+                                                                                true);
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            logger.error("Removal of timestamped Maven snapshot artifact: " + artifactPath + ".");
+        }
 
     }
 
-//    private void removeTimestampedSnapshot (String basePath, Repository repository, int numberToKeep, int keepPeriod)
-//            throws ProviderImplementationException, IOException, XmlPullParserException
-//    {
-//        String storageId = repository.getStorage().getId();
-//        File file = new File(basePath);
-//
-//        LayoutProvider layoutProvider = getLayoutProvider(repository, layoutProviderRegistry);
-//
-//        Metadata metadata = mavenMetadataManager.readMetadata(Paths.get(basePath));
-//        int numberToRemove;
-//
-//        if (metadata != null && metadata.getVersioning() != null)
-//        {
-//            Snapshot snapshot = metadata.getVersioning().getSnapshot();
-//
-//            if (numberToKeep != 0 && numberToKeep < snapshot.getBuildNumber()) {
-//                numberToRemove = snapshot.getBuildNumber() - numberToKeep;
-//            }
-//            else if (numberToKeep == 0 && keepPeriod != 0)
-//            {
-//                SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd.HHmmss");
-//                Calendar calendar = Calendar.getInstance();
-//                Date buildDate = formatter.(calendar.getTime())
-//            }
-//            metadata.getVersioning()
-//                    .getVersions()
-//                    .remove(version);
-//            mavenMetadataManager.storeMetadata(path, null, metadata, MetadataType.ARTIFACT_ROOT_LEVEL);
-//        }
-//
-//        List<File> list = Arrays.asList(file.listFiles());
-//
-//        list.stream().filter(File::isFile)
-//                     .filter(e -> ArtifactUtils.isArtifact(e.getPath()))
-//                     .forEach(e ->
-//                                        {
-//                                            if (allowDeletingTimestampedSnapshot(e, numberToKeep, keepPeriod))
-//                                            {
-//                                                try
-//                                                {
-//
-//                                                    layoutProvider.delete(storageId, repository.getId(), e.getPath(), true);
-//
-//                                                    String artifactName = e.getName();
-//                                                    artifactName = artifactName.substring(0, artifactName.lastIndexOf("."));
-//                                                    String pomPath = Paths.get(e.getParent(), artifactName).toString().concat(".pom");
-//
-//                                                    layoutProvider.delete(storageId, repository.getId(), pomPath, true);
-//                                                }
-//                                                catch (IOException e1)
-//                                                {
-//                                                    logger.error(e1.getMessage(), e1);
-//                                                }
-//
-//                                            }
-//                                        });
-//
-//    }
-//
-//    private boolean allowDeletingTimestampedSnapshot (File artifactFile, int numberToKeep, int keepPeriod)
-//    {
-//        Artifact artifact = ArtifactUtils.convertPathToArtifact(artifactFile.getPath());
-//
-//        Metadata metadata = mavenMetadataManager.readMetadata()
-//        return true;
-//    }
+    private boolean removeTimestampedSnapshot(String basePath,
+                                              Repository repository,
+                                              int numberToKeep,
+                                              int keepPeriod)
+            throws ProviderImplementationException, IOException, XmlPullParserException, ParseException
+    {
+        String storageId = repository.getStorage()
+                                     .getId();
+        File file = new File(basePath);
+
+        LayoutProvider layoutProvider = getLayoutProvider(repository, layoutProviderRegistry);
+
+        Metadata metadata = mavenMetadataManager.readMetadata(Paths.get(basePath));
+
+        if (metadata != null && metadata.getVersioning() != null)
+        {
+            /**
+             * map of snapshots for removing
+             * k - number of the build, v - version of the snapshot
+             */
+            Map<Integer, String> mapToRemove = getRemovingSnapshots(basePath, metadata, numberToKeep, keepPeriod);
+
+            if (!mapToRemove.isEmpty())
+            {
+                List<String> removingSnapshots = new ArrayList<>();
+
+                new ArrayList<>(mapToRemove.values()).forEach(e -> removingSnapshots.add(metadata.getArtifactId()
+                                                                                                 .concat("-")
+                                                                                                 .concat(e)
+                                                                                                 .concat(".jar"))
+                );
+
+                List<File> list = Arrays.asList(file.listFiles());
+
+                list.stream()
+                    .filter(File::isFile)
+                    .filter(e -> ArtifactUtils.isArtifact(e.getPath()) && removingSnapshots.contains(e.getName()))
+                    .forEach(e ->
+                             {
+                                 try
+                                 {
+
+                                     layoutProvider.delete(storageId, repository.getId(), e.getPath(), true);
+
+                                     String artifactName = e.getName();
+                                     artifactName = artifactName.substring(0, artifactName.lastIndexOf("."));
+                                     String pomPath = Paths.get(e.getParent(), artifactName)
+                                                           .toString()
+                                                           .concat(".pom");
+
+                                     layoutProvider.delete(storageId, repository.getId(), pomPath, true);
+                                 }
+                                 catch (IOException e1)
+                                 {
+                                     logger.error(e1.getMessage(), e1);
+                                 }
+
+                             });
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    private Map<Integer, String> getRemovingSnapshots(String basePath,
+                                                      Metadata metadata,
+                                                      int numberToKeep,
+                                                      int keepPeriod)
+            throws IOException, XmlPullParserException
+    {
+        /**
+         * map of the snapshots in metadata file
+         * k - number of the build, v - version of the snapshot
+         */
+        Map<Integer, String> snapshots = new HashMap<>();
+        /**
+         * map of snapshots for removing
+         * k - number of the build, v - version of the snapshot
+         */
+        Map<Integer, String> mapToRemove = new HashMap<>();
+
+        metadata.getVersioning()
+                .getSnapshotVersions()
+                .forEach(e ->
+                         {
+
+                             if (e.getExtension()
+                                  .equals("jar"))
+                             {
+                                 String version = e.getVersion();
+                                 snapshots.put(Integer.parseInt(ArtifactUtils.getSnapshotBuildNumber(version)),
+                                               version);
+                             }
+                         });
+
+        if (numberToKeep != 0 && snapshots.size() > numberToKeep)
+        {
+            snapshots.forEach(((k, v) ->
+                               {
+                                   int count = 0;
+                                   if (count != snapshots.size() - numberToKeep)
+                                   {
+                                       mapToRemove.put(k, v);
+                                       count++;
+                                   }
+                               }));
+        }
+        else if (numberToKeep == 0 && keepPeriod != 0)
+        {
+            snapshots.forEach(((k, v) ->
+                               {
+                                   try
+                                   {
+                                       if (keepPeriod < getDifferenceDays(ArtifactUtils.getSnapshotTimestamp(v)))
+                                       {
+                                           mapToRemove.put(k, v);
+                                       }
+                                   }
+                                   catch (ParseException e)
+                                   {
+                                       logger.error(e.getMessage(), e);
+                                   }
+                               }));
+        }
+
+        return mapToRemove;
+    }
+
+    private int getDifferenceDays(String buildTimestamp)
+            throws ParseException
+    {
+        DateFormat formatter = new SimpleDateFormat(TIMESTAMP_FORMAT);
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = formatter.format(calendar.getTime());
+        Date d2 = formatter.parse(currentDate);
+        Date d1 = formatter.parse(buildTimestamp);
+        long diff = d2.getTime() - d1.getTime();
+        return (int) diff / (24 * 60 * 60 * 1000);
+    }
 
 }
