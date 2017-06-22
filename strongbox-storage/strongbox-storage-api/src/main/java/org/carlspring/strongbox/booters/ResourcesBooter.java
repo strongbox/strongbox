f4d964b76bf070d281bcb249ab27ecf79331ab42
carlspring <carlspring@gmail.com>
2017-06-22 17:12:31 +0100

    SB-818: Sort out the strongbox-assembly issues
    Sonar fixes.

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/booters/ResourcesBooter.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/booters/ResourcesBooter.java
@@ -7,8 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
