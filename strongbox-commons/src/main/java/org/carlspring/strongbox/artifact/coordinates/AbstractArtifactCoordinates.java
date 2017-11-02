d215522b7e082573c1d195fb7340ac608c37fb74
sbespalov <serge.bespalov@gmail.com>
2017-11-02 08:14:25 +0600

    SB-906: cosmetic

--- strongbox-commons/src/main/java/org/carlspring/strongbox/artifact/coordinates/AbstractArtifactCoordinates.java
+++ strongbox-commons/src/main/java/org/carlspring/strongbox/artifact/coordinates/AbstractArtifactCoordinates.java
@@ -5,7 +5,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.carlspring.strongbox.data.domain.GenericEntity;
-import org.carlspring.strongbox.data.domain.GenericEntityHook;
 
 
 /**
