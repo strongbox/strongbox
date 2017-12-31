5bbf6b37bf8ba6ca11fa04b238eefe8f49dfb0ed
carlspring <carlspring@gmail.com>
2017-12-31 06:20:56 +0200

    SB-749: Implement a raw layout provider
    - Removed a duplicate dependency.
    - Slight tidying of the `strongbox.xml`.

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-nuget-layout-provider/src/test/java/org/carlspring/strongbox/storage/indexing/NugetRemoteRepositoryTest.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-nuget-layout-provider/src/test/java/org/carlspring/strongbox/storage/indexing/NugetRemoteRepositoryTest.java
@@ -17,7 +17,6 @@ import org.carlspring.strongbox.config.NugetLayoutProviderTestConfig;
 import org.carlspring.strongbox.configuration.ConfigurationManager;
 import org.carlspring.strongbox.domain.ArtifactEntry;
 import org.carlspring.strongbox.domain.RemoteArtifactEntry;
-import org.carlspring.strongbox.providers.ProviderImplementationException;
 import org.carlspring.strongbox.repository.NugetRepositoryFeatures;
 import org.carlspring.strongbox.services.ArtifactEntryService;
 import org.carlspring.strongbox.services.RepositoryManagementService;
@@ -46,7 +45,8 @@ public class NugetRemoteRepositoryTest
         extends TestCaseWithRepository
 {
 
-    private static final String NUGET_COMMON_STORAGE = "nuget-common-storage";
+    private static final String NUGET_COMMON_STORAGE = "storage-nuget";
+
     private static final String REPOSITORY_PROXY = "nrrt-proxy";
 
     @Inject
@@ -60,7 +60,8 @@ public class NugetRemoteRepositoryTest
 
     @Inject
     private NugetRepositoryFeatures features;
-    
+
+
     @BeforeClass
     public static void cleanUp()
         throws Exception
@@ -106,20 +107,22 @@ public class NugetRemoteRepositoryTest
 
     @Test
     public void testRepositoryIndexFetching()
-        throws ArtifactTransportException,
-        IOException, ProviderImplementationException
+            throws ArtifactTransportException, IOException
     {
         Storage storage = configurationManager.getConfiguration().getStorage(NUGET_COMMON_STORAGE);
         Repository repository = storage.getRepository(REPOSITORY_PROXY);
 
-        ((NugetRepositoryFeatures) features).downloadRemoteFeed(storage.getId(),
-                                                                repository.getId(),
-                                                                new IdEqIgnoreCase("NHibernate"),
-                                                                null,
-                                                                null);
+        features.downloadRemoteFeed(storage.getId(),
+                                    repository.getId(),
+                                    new IdEqIgnoreCase("NHibernate"),
+                                    null,
+                                    null);
 
         NugetArtifactCoordinates c = new NugetArtifactCoordinates("NHibernate", "4.0.4.4000", "nupkg");
-        Optional<ArtifactEntry> artifactEntry = artifactEntryService.findOneArtifact(NUGET_COMMON_STORAGE, REPOSITORY_PROXY, c.toPath());
+        Optional<ArtifactEntry> artifactEntry = artifactEntryService.findOneArtifact(NUGET_COMMON_STORAGE,
+                                                                                     REPOSITORY_PROXY,
+                                                                                     c.toPath());
+
         assertTrue(artifactEntry.isPresent());
         assertFalse(((RemoteArtifactEntry)artifactEntry.get()).getIsCached());
     }
