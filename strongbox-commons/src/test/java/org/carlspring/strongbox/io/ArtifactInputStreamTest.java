960fe98f7f9d2011168f40f5e9409e113b5de75c
Dinesh Arora <dinesh19aug@gmail.com>
2018-01-01 23:54:20 -0500

    Added another test case

--- strongbox-commons/src/test/java/org/carlspring/strongbox/io/ArtifactInputStreamTest.java
+++ strongbox-commons/src/test/java/org/carlspring/strongbox/io/ArtifactInputStreamTest.java
@@ -14,23 +14,41 @@ public class ArtifactInputStreamTest
 
     ArtifactInputStream artifactInputStream ;
     InputStream inputStream;
+    String pathTxt, pathSh;
 
     @Before
     public void setUp()
-            throws FileNotFoundException, NoSuchAlgorithmException
+
     {
-        String path = "src/test/resources/testTextFile.txt";
-        inputStream = new BufferedInputStream(new FileInputStream(path.trim()));
-        artifactInputStream = new ArtifactInputStream(null, inputStream){};
+         pathTxt = "src/test/resources/testTextFile.txt";
+         pathSh = "src/test/resources/project-1.4.1.tar.gz";
+
    }
 
 
     @Test
-    public void testFileExtensionEqualsTxt()
+    public void testFileExtensionEqualsTxt()  throws FileNotFoundException, NoSuchAlgorithmException
     {
         try
         {
+            inputStream = new BufferedInputStream(new FileInputStream(pathTxt.trim()));
+            artifactInputStream = new ArtifactInputStream(null, inputStream){};
             assertTrue(artifactInputStream.getFileExtension(IOUtils.toByteArray(inputStream)).equalsIgnoreCase(".txt"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testFileExtensionEqualsGz()  throws FileNotFoundException, NoSuchAlgorithmException
+    {
+        try
+        {
+            inputStream = new BufferedInputStream(new FileInputStream(pathSh.trim()));
+            artifactInputStream = new ArtifactInputStream(null, inputStream){};
+            assertTrue(artifactInputStream.getFileExtension(IOUtils.toByteArray(inputStream)).equalsIgnoreCase(".gz"));
         }
         catch (IOException e)
         {
