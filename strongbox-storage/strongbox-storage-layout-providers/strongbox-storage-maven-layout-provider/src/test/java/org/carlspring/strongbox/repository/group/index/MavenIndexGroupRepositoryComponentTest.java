8b0f192fb6aa9a93c72cfddf0fa853df3b2ade7e
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2018-01-27 21:08:33 +0100

    SB-1038

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/repository/group/index/MavenIndexGroupRepositoryComponentTest.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/repository/group/index/MavenIndexGroupRepositoryComponentTest.java
@@ -46,7 +46,6 @@ public class MavenIndexGroupRepositoryComponentTest
         artifactIndexesService.rebuildIndexes();
     }
 
-    @Ignore
     @Test
     public void artifactDeletionShouldDeleteArtifactFromParentGroupRepositoryIndex()
             throws Exception
