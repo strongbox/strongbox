ed4321f6be334143e16230f1c599d10c9b1884ec
Pablo Tirado <pablotr87@gmail.com>
2018-02-01 21:25:38 +0100

    Make sure CorsConfigurationController is following the "How To Write Controllers" guide

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/security/cors/CorsConfigurationController.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/security/cors/CorsConfigurationController.java
@@ -4,6 +4,7 @@ import org.carlspring.strongbox.controllers.BaseController;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,18 +12,19 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * @author Przemyslaw Fusik
+ * @author Pablo Tirado
  */
 @RestController
 @PreAuthorize("hasAuthority('ADMIN')")
@@ -35,39 +37,53 @@ public class CorsConfigurationController
     @Inject
     private CorsConfigurationSource corsConfigurationSource;
 
-    @ApiOperation(value = "Returns allowed origins", httpMethod = "GET", produces = MediaType.APPLICATION_XML_VALUE +
-                                                                                    "," +
-                                                                                    MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Returns allowed origins")
     @ApiResponses(value = @ApiResponse(code = 200, message = "Allowed origins."))
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces = { MediaType.APPLICATION_XML_VALUE,
-                                                                          MediaType.APPLICATION_JSON_VALUE })
-    public ResponseEntity getAllowedOrigins()
+    @GetMapping(produces = { MediaType.TEXT_PLAIN_VALUE,
+                             MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity getAllowedOrigins(@RequestHeader(HttpHeaders.ACCEPT) String accept)
     {
-        return ResponseEntity.ok(
-                ((UrlBasedCorsConfigurationSource) corsConfigurationSource).getCorsConfigurations()
-                                                                           .values()
-                                                                           .stream()
-                                                                           .flatMap(c -> c.getAllowedOrigins() != null ?
-                                                                                         c.getAllowedOrigins().stream() :
-                                                                                         Stream.empty())
-                                                                           .collect(Collectors.toList()));
+        List<String> allowedOrigins = ((UrlBasedCorsConfigurationSource) corsConfigurationSource).getCorsConfigurations()
+                                                                                                 .values()
+                                                                                                 .stream()
+                                                                                                 .flatMap(
+                                                                                                         c -> c.getAllowedOrigins() !=
+                                                                                                              null ?
+                                                                                                              c.getAllowedOrigins().stream() :
+                                                                                                              Stream.empty())
+                                                                                                 .collect(
+                                                                                                         Collectors.toList());
+        return ResponseEntity.ok(getListResponseEntityBody(allowedOrigins, accept));
     }
 
-    @ApiOperation(value = "Sets CORS allowed origins", httpMethod = "PUT", consumes = MediaType.APPLICATION_JSON_VALUE,
+    @ApiOperation(value = "Sets CORS allowed origins",
             notes = "In the request body, put an array of all allowed origins like this example: " +
                     "[\"http://dev.carlspring.org/confluence\",\"http://dev.carlspring.org/jenkins\"]. " +
                     "You can always provide [*] to allow all origins.")
-    @ApiResponses(value = @ApiResponse(code = 200, message = "CORS allowed origins set succeeded"))
-    @RequestMapping(value = "/", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity setAllowedOrigins(@RequestBody List<String> allowedOrigins)
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "CORS allowed origins was updated."),
+                            @ApiResponse(code = 400, message = "Could not update CORS allowed origins.") })
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+                produces = { MediaType.TEXT_PLAIN_VALUE,
+                             MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity setAllowedOrigins(@RequestBody List<String> allowedOrigins,
+                                            @RequestHeader(HttpHeaders.ACCEPT) String accept)
     {
-        ((UrlBasedCorsConfigurationSource) corsConfigurationSource).getCorsConfigurations()
-                                                                   .values()
-                                                                   .stream()
-                                                                   .findFirst()
-                                                                   .get()
-                                                                   .setAllowedOrigins(allowedOrigins);
+        Optional<CorsConfiguration> corsConfiguration = ((UrlBasedCorsConfigurationSource) corsConfigurationSource).getCorsConfigurations()
+                                                                                                                   .values()
+                                                                                                                   .stream()
+                                                                                                                   .findFirst();
 
-        return ResponseEntity.ok("CORS allowed origins set succeeded");
+        if (corsConfiguration.isPresent())
+        {
+            corsConfiguration.get().setAllowedOrigins(allowedOrigins);
+            return ResponseEntity.ok(getResponseEntityBody("CORS allowed origins was updated.", accept));
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body(getResponseEntityBody("Could not update CORS allowed origins.", accept));
+        }
+
+
     }
 }
--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/security/cors/CorsConfigurationControllerTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/security/cors/CorsConfigurationControllerTest.java
@@ -5,25 +5,30 @@ import org.carlspring.strongbox.rest.common.RestAssuredBaseTest;
 
 import javax.inject.Inject;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 /**
  * @author Przemyslaw Fusik
+ * @author Pablo Tirado
  */
 @IntegrationTest
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 public class CorsConfigurationControllerTest
         extends RestAssuredBaseTest
 {
@@ -36,7 +41,8 @@ public class CorsConfigurationControllerTest
     @Before
     public void before()
     {
-        initialConfiguration = new HashMap<>(((UrlBasedCorsConfigurationSource) corsConfigurationSource).getCorsConfigurations());
+        initialConfiguration = new HashMap<>(
+                ((UrlBasedCorsConfigurationSource) corsConfigurationSource).getCorsConfigurations());
     }
 
     @After
@@ -46,88 +52,130 @@ public class CorsConfigurationControllerTest
     }
 
     @Test
-    public void shouldReturnExpectedAllowedOrigins()
+    public void shouldReturnExpectedAllowedOriginsWithTextAcceptHeader()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/configuration/cors/")
-                          .peek()
-                          .then()
-                          .statusCode(200)
-                          .body(CoreMatchers.equalTo("[ ]"));
+        String url = "/configuration/cors";
+
+        given().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+               .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN_VALUE)
+               .body(Collections.emptyList())
+               .when()
+               .put(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body(equalTo("CORS allowed origins was updated."));
+
+        given().header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN_VALUE)
+               .when()
+               .get(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body(equalTo("[]"));
+    }
+
+    @Test
+    public void shouldReturnExpectedAllowedOriginsWithJsonAcceptHeader()
+            throws Exception
+    {
+        String url = "/configuration/cors";
+
+        given().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+               .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .body(Collections.emptyList())
+               .when()
+               .put(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body("message", equalTo("CORS allowed origins was updated."));
+
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body(equalTo("[ ]"));
     }
 
     @Test
     public void shouldAllowOneOrigin()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Content-Type", "application/json")
-                          .body(Arrays.asList("http://example.com"))
-                          .when()
-                          .put("/configuration/cors/")
-                          .peek()
-                          .then()
-                          .statusCode(200);
+        String url = "/configuration/cors";
 
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/configuration/cors/")
-                          .peek()
-                          .then()
-                          .statusCode(200)
-                          .body(CoreMatchers.equalTo("[ \"http://example.com\" ]"));
+        given().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+               .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .body(Collections.singletonList("http://example.com"))
+               .when()
+               .put(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body("message", equalTo("CORS allowed origins was updated."));
+
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body(equalTo("[ \"http://example.com\" ]"));
     }
 
     @Test
     public void shouldAllowAllOrigins()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Content-Type", "application/json")
-                          .body(Arrays.asList("*"))
-                          .when()
-                          .put("/configuration/cors/")
-                          .peek()
-                          .then()
-                          .statusCode(200);
+        String url = "/configuration/cors";
 
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/configuration/cors/")
-                          .peek()
-                          .then()
-                          .statusCode(200)
-                          .body(CoreMatchers.equalTo("[ \"*\" ]"));
+        given().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+               .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN_VALUE)
+               .body(Collections.singletonList("*"))
+               .when()
+               .put(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body(equalTo("CORS allowed origins was updated."));
+
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body(equalTo("[ \"*\" ]"));
     }
 
     @Test
     public void shouldAllowMultipleOrigins()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Content-Type", "application/json")
-                          .body(Arrays.asList("http://example.com", "https://google.com",
-                                              "http://dev.carlspring.org/confluence",
-                                              "http://dev.carlspring.org/jenkins"))
-                          .when()
-                          .put("/configuration/cors/")
-                          .peek()
-                          .then()
-                          .statusCode(200);
+        String url = "/configuration/cors";
 
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/configuration/cors/")
-                          .peek()
-                          .then()
-                          .statusCode(200)
-                          .body(CoreMatchers.equalTo(
-                                  "[ \"http://example.com\", \"https://google.com\", \"http://dev.carlspring.org/confluence\", \"http://dev.carlspring.org/jenkins\" ]"));
+        given().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+               .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .body(Arrays.asList("http://example.com", "https://google.com",
+                                   "http://dev.carlspring.org/confluence",
+                                   "http://dev.carlspring.org/jenkins"))
+               .when()
+               .put(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body("message", equalTo("CORS allowed origins was updated."));
+
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(url)
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body(equalTo(
+                       "[ \"http://example.com\", \"https://google.com\", \"http://dev.carlspring.org/confluence\", \"http://dev.carlspring.org/jenkins\" ]"));
     }
 }
