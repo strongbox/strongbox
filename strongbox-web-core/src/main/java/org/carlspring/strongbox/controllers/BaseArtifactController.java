4875aa50e4b79922051ed803c2f533275320190d
Bagdat Bimaganbetov <b.bimaganbetov@innopolis.ru>
2018-02-12 13:16:15 +0300

    Fixed pairing of HTML tags

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/BaseArtifactController.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/BaseArtifactController.java
@@ -206,9 +206,9 @@ public abstract class BaseArtifactController
             sb.append("<th>Size</th>");
             sb.append("<th>Description</th>");
             sb.append("</tr>");
-            sb.append("<tr>");
             if (!request.getRequestURI().equalsIgnoreCase(BrowseStoragesController.ROOT_CONTEXT + "/"))
             {
+                sb.append("<tr>");
                 sb.append("<td colspan=4><a href=\"../\">..</a></td>");
                 sb.append("</tr>");
             }
