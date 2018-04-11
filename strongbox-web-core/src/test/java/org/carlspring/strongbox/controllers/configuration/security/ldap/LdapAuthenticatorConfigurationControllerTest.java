e187ba29c86c85950fa1e5d15f90faa057df709f
Pablo Tirado <pablotr87@gmail.com>
2018-04-11 19:10:40 +0200

    Issue 682: Make sure the LdapAuthenticatorConfigurationController is following the How-To-Write-Controllers guide

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/configuration/security/ldap/LdapAuthenticatorConfigurationControllerTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/configuration/security/ldap/LdapAuthenticatorConfigurationControllerTest.java
@@ -6,18 +6,23 @@ import org.carlspring.strongbox.rest.common.RestAssuredBaseTest;
 
 import javax.inject.Inject;
 
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 /**
  * @author Przemyslaw Fusik
+ * @author Pablo Tirado
  */
 @IntegrationTest
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 public class LdapAuthenticatorConfigurationControllerTest
         extends RestAssuredBaseTest
 {
@@ -29,234 +34,231 @@ public class LdapAuthenticatorConfigurationControllerTest
     public void setUp()
             throws Exception
     {
+        setContextBaseUrl("/api/configuration/ldap");
         scanner.scanAndReloadRegistry();
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void shouldReturnProperRolesMapping()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/api/configuration/ldap/rolesMapping")
-                          .peek()
-                          .then()
-                          .body("roles-mapping.Developers",
-                                CoreMatchers.equalTo("REPOSITORY_MANAGER"))
-                          .body("roles-mapping.Contributors",
-                                CoreMatchers.equalTo("USER_ROLE"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/rolesMapping")
+               .peek()
+               .then()
+               .body("roles-mapping.Developers", equalTo("REPOSITORY_MANAGER"))
+               .body("roles-mapping.Contributors", equalTo("USER_ROLE"))
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void userDnPatternsContainExpectedUserDnPattern()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/api/configuration/ldap/userDnPatterns")
-                          .peek()
-                          .then()
-                          .body("user-dn-pattern[0]",
-                                CoreMatchers.equalTo("uid={0},ou=Users"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/userDnPatterns")
+               .peek()
+               .then()
+               .body("user-dn-pattern[0]", equalTo("uid={0},ou=Users"))
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void userSearchFilterEqualsExpectedValue()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/api/configuration/ldap/userSearchFilter")
-                          .peek()
-                          .then()
-                          .body("search-filter",
-                                CoreMatchers.equalTo("(uid={0})"))
-                          .body("search-base",
-                                CoreMatchers.equalTo("ou=people"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/userSearchFilter")
+               .peek()
+               .then()
+               .body("search-filter", equalTo("(uid={0})"))
+               .body("search-base", equalTo("ou=people"))
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void groupSearchFilterEqualsExpectedValue()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/api/configuration/ldap/groupSearchFilter")
-                          .peek()
-                          .then()
-                          .body("search-base",
-                                CoreMatchers.equalTo("ou=Groups"))
-                          .body("search-filter",
-                                CoreMatchers.equalTo("(uniqueMember={0})"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/groupSearchFilter")
+               .peek()
+               .then()
+               .body("search-base", equalTo("ou=Groups"))
+               .body("search-filter", equalTo("(uniqueMember={0})"))
+               .statusCode(HttpStatus.OK.value());
     }
 
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void shouldUpdateExistingMapping()
             throws Exception
     {
-        RestAssuredMockMvc.when()
-                          .put("/api/configuration/ldap/rolesMapping/Developers/REPOSITORY_READER")
-                          .peek()
-                          .then()
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .put(getContextBaseUrl() + "/rolesMapping/Developers/REPOSITORY_READER")
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void shouldAddNewMapping()
             throws Exception
     {
-        RestAssuredMockMvc.when()
-                          .put("/api/configuration/ldap/rolesMapping/Managers/REPOSITORY_MANAGER")
-                          .peek()
-                          .then()
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .put(getContextBaseUrl() + "/rolesMapping/Managers/REPOSITORY_MANAGER")
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void shouldNotAllowToAddNewMappingOnExistingKey()
             throws Exception
     {
-        RestAssuredMockMvc.when()
-                          .post("/api/configuration/ldap/rolesMapping/Contributors/REPOSITORY_READER")
-                          .peek()
-                          .then()
-                          .statusCode(400);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .post(getContextBaseUrl() + "/rolesMapping/Contributors/REPOSITORY_READER")
+               .peek()
+               .then()
+               .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void shouldAllowToAddNewMappingOnNotExistingKey()
             throws Exception
     {
-        RestAssuredMockMvc.when()
-                          .post("/api/configuration/ldap/rolesMapping/Testers/REPOSITORY_READER")
-                          .peek()
-                          .then()
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .post(getContextBaseUrl() + "/rolesMapping/Testers/REPOSITORY_READER")
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void shouldAllowToDeleteExistingMapping()
             throws Exception
     {
-        RestAssuredMockMvc.when()
-                          .delete("/api/configuration/ldap/rolesMapping/Contributors")
-                          .peek()
-                          .then()
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .delete(getContextBaseUrl() + "/rolesMapping/Contributors")
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void shouldNotAllowToDeleteNotExistingMapping()
             throws Exception
     {
-        RestAssuredMockMvc.when()
-                          .delete("/api/configuration/ldap/rolesMapping/Testers")
-                          .peek()
-                          .then()
-                          .statusCode(400);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .delete(getContextBaseUrl() + "/rolesMapping/Testers")
+               .peek()
+               .then()
+               .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void userSearchFilterShouldBeUpdatable()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/xml")
-                          .when()
-                          // below line is a workaround URI placeholders in RestAssuredMockMvc
-                          .put("/api/configuration/ldap/userSearchFilter/ou=guys/(uid={0})", "{0}")
-                          .peek()
-                          .then()
-                          .body(CoreMatchers.equalTo("User search filter updated."))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               // below line is a workaround URI placeholders in RestAssuredMockMvc
+               .put(getContextBaseUrl() + "/userSearchFilter/ou=guys/(uid={0})", "{0}")
+               .peek()
+               .then()
+               .body(containsString("User search filter updated."))
+               .statusCode(HttpStatus.OK.value());
 
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/api/configuration/ldap/userSearchFilter")
-                          .peek()
-                          .then()
-                          .body("search-filter",
-                                CoreMatchers.equalTo("(uid={0})"))
-                          .body("search-base",
-                                CoreMatchers.equalTo("ou=guys"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/userSearchFilter")
+               .peek()
+               .then()
+               .body("search-filter", equalTo("(uid={0})"))
+               .body("search-base", equalTo("ou=guys"))
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void groupSearchFilterShouldBeUpdatable()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          // below line is a workaround URI placeholders in RestAssuredMockMvc
-                          .put("/api/configuration/ldap/groupSearchFilter/ou=guys/(participiant={0})", "{0}")
-                          .peek()
-                          .then()
-                          .body(CoreMatchers.equalTo("Group search filter updated."))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               // below line is a workaround URI placeholders in RestAssuredMockMvc
+               .put(getContextBaseUrl() + "/groupSearchFilter/ou=guys/(participiant={0})", "{0}")
+               .peek()
+               .then()
+               .body(containsString("Group search filter updated."))
+               .statusCode(HttpStatus.OK.value());
 
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/api/configuration/ldap/groupSearchFilter")
-                          .peek()
-                          .then()
-                          .body("search-base",
-                                CoreMatchers.equalTo("ou=guys"))
-                          .body("search-filter",
-                                CoreMatchers.equalTo("(participiant={0})"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/groupSearchFilter")
+               .peek()
+               .then()
+               .body("search-base", equalTo("ou=guys"))
+               .body("search-filter", equalTo("(participiant={0})"))
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void userDnPatternsAreRemovable()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          // below line is a workaround URI placeholders in RestAssuredMockMvc
-                          .delete("/api/configuration/ldap/userDnPatterns/uid={0},ou=Users", "{0}")
-                          .peek()
-                          .then()
-                          .body(CoreMatchers.equalTo(
-                                  "user DN pattern uid={0},ou=Users removed from the userDnPatterns"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               // below line is a workaround URI placeholders in RestAssuredMockMvc
+               .delete(getContextBaseUrl() + "/userDnPatterns/uid={0},ou=Users", "{0}")
+               .peek()
+               .then()
+               .body(containsString("User DN pattern uid={0},ou=Users removed from the userDnPatterns"))
+               .statusCode(HttpStatus.OK.value());
     }
 
+    @WithMockUser(authorities = "ADMIN")
     @Test
     public void userDnPatternsAreAdditable()
             throws Exception
     {
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/xml")
-                          .when()
-                          // below line is a workaround URI placeholders in RestAssuredMockMvc
-                          .post("/api/configuration/ldap/userDnPatterns/uid={0},ou=Guys", "{0}")
-                          .peek()
-                          .then()
-                          .body(CoreMatchers.equalTo(
-                                  "user DN pattern uid={0},ou=Guys added to the userDnPatterns"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               // below line is a workaround URI placeholders in RestAssuredMockMvc
+               .post(getContextBaseUrl() + "/userDnPatterns/uid={0},ou=Guys", "{0}")
+               .peek()
+               .then()
+               .body(containsString("User DN pattern uid={0},ou=Guys added to the userDnPatterns"))
+               .statusCode(HttpStatus.OK.value());
 
-        RestAssuredMockMvc.given()
-                          .header("Accept", "application/json")
-                          .when()
-                          .get("/api/configuration/ldap/userDnPatterns")
-                          .peek()
-                          .then()
-                          .body("user-dn-pattern[0]",
-                                CoreMatchers.equalTo("uid={0},ou=Users"))
-                          .body("user-dn-pattern[1]",
-                                CoreMatchers.equalTo("uid={0},ou=Guys"))
-                          .statusCode(200);
+        given().accept(MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/userDnPatterns")
+               .peek()
+               .then()
+               .body("user-dn-pattern[0]", equalTo("uid={0},ou=Users"))
+               .body("user-dn-pattern[1]", equalTo("uid={0},ou=Guys"))
+               .statusCode(HttpStatus.OK.value());
     }
 
 }
