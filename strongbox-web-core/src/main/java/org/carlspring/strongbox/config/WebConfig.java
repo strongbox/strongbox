a498e7098143d42a85b976dcde1feb1f37cd3f6a
Pablo Tirado <pablotr87@gmail.com>
2018-04-16 21:53:18 +0200

    Issue 681: Refactor ProxyConfiguration to use Form Validation and make sure it follows the How-To-Write-Controllers guide

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/config/WebConfig.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.carlspring.strongbox.configuration.StrongboxSecurityConfig;
 import org.carlspring.strongbox.converters.PrivilegeListFormToPrivilegeListConverter;
 import org.carlspring.strongbox.converters.RoleFormToRoleConverter;
 import org.carlspring.strongbox.converters.RoleListFormToRoleListConverter;
+import org.carlspring.strongbox.converters.configuration.ProxyConfigurationFormToProxyConfigurationConverter;
 import org.carlspring.strongbox.converters.users.AccessModelFormToAccessModelConverter;
 import org.carlspring.strongbox.converters.users.UserFormToUserConverter;
 import org.carlspring.strongbox.cron.config.CronTasksConfig;
@@ -187,6 +188,7 @@ public class WebConfig
         registry.addConverter(new PrivilegeListFormToPrivilegeListConverter());
         registry.addConverter(new UserFormToUserConverter());
         registry.addConverter(new AccessModelFormToAccessModelConverter());
+        registry.addConverter(new ProxyConfigurationFormToProxyConfigurationConverter());
     }
 
     @Bean
---
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/forms/configuration/ProxyConfigurationForm.java
@@ -0,0 +1,95 @@
+package org.carlspring.strongbox.forms.configuration;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+/**
+ * @author Pablo Tirado
+ */
+public class ProxyConfigurationForm
+{
+
+    @NotBlank(message = "A host must be specified.")
+    private String host;
+
+    @Min(value = 1, message = "Port number must be an integer between 1 and 65535.")
+    @Max(value = 65535, message = "Port number must be an integer between 1 and 65535.")
+    private int port;
+
+    @NotBlank(message = "A proxy type must be specified.")
+    @Pattern(regexp = "DIRECT|HTTP|SOCKS4|SOCKS5",
+             flags = Pattern.Flag.CASE_INSENSITIVE,
+             message = "Proxy type must contain one the following strings as value: DIRECT, HTTP, SOCKS4, SOCKS5")
+    private String type;
+
+    private String username;
+
+    private String password;
+
+    private List<String> nonProxyHosts = Lists.newArrayList();
+
+    public String getHost()
+    {
+        return host;
+    }
+
+    public void setHost(String host)
+    {
+        this.host = host;
+    }
+
+    public int getPort()
+    {
+        return port;
+    }
+
+    public void setPort(int port)
+    {
+        this.port = port;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public List<String> getNonProxyHosts()
+    {
+        return nonProxyHosts;
+    }
+
+    public void setNonProxyHosts(List<String> nonProxyHosts)
+    {
+        this.nonProxyHosts = nonProxyHosts;
+    }
+}
