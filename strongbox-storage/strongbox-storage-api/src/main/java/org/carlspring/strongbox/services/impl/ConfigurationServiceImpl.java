9201e9a320cb098b32c182e828b9d9d976f004ad
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2018-05-23 00:48:41 +0200

    SB-1119

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/services/impl/ConfigurationServiceImpl.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/services/impl/ConfigurationServiceImpl.java
@@ -1,10 +1,12 @@
 package org.carlspring.strongbox.services.impl;
 
 import org.carlspring.strongbox.configuration.Configuration;
+import org.carlspring.strongbox.data.criteria.DetachQueryTemplate;
 import org.carlspring.strongbox.data.service.SingletonCommonCrudService;
 import org.carlspring.strongbox.services.ConfigurationService;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -24,4 +26,14 @@ public class ConfigurationServiceImpl
         return Configuration.class;
     }
 
+    @Override
+    public Optional<Configuration> findOne()
+    {
+        Optional<Configuration> result = super.findOne();
+        if (!result.isPresent())
+        {
+            return result;
+        }
+        return Optional.of((Configuration) new DetachQueryTemplate(entityManager).unproxy(result.get()));
+    }
 }
