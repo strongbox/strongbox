22739998b07efc78527ac3297deadfc80b79a243
sbespalov <serge.bespalov@gmail.com>
2018-05-23 16:20:58 +0700

    SB-571: AQL Controller

--- strongbox-aql/src/main/java/org/carlspring/strongbox/services/impl/AqlSearchServiceImpl.java
+++ strongbox-aql/src/main/java/org/carlspring/strongbox/services/impl/AqlSearchServiceImpl.java
@@ -3,6 +3,7 @@ package org.carlspring.strongbox.services.impl;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -13,10 +14,12 @@ import org.carlspring.strongbox.data.criteria.DetachQueryTemplate;
 import org.carlspring.strongbox.data.criteria.OQueryTemplate;
 import org.carlspring.strongbox.data.criteria.QueryTemplate;
 import org.carlspring.strongbox.data.criteria.Selector;
+import org.carlspring.strongbox.dependency.snippet.SnippetGenerator;
 import org.carlspring.strongbox.domain.ArtifactEntry;
 import org.carlspring.strongbox.providers.io.RepositoryPath;
 import org.carlspring.strongbox.services.AqlSearchService;
 import org.carlspring.strongbox.services.ArtifactResolutionService;
+import org.carlspring.strongbox.storage.repository.Repository;
 import org.carlspring.strongbox.storage.search.SearchResult;
 import org.carlspring.strongbox.storage.search.SearchResults;
 import org.springframework.stereotype.Component;
@@ -32,6 +35,9 @@ public class AqlSearchServiceImpl implements AqlSearchService
     @Inject
     private ArtifactResolutionService artifactResolutionService;
 
+    @Inject
+    private SnippetGenerator snippetGenerator;
+
     public SearchResults search(Selector<ArtifactEntry> selector)
         throws IOException
     {
@@ -45,14 +51,21 @@ public class AqlSearchServiceImpl implements AqlSearchService
             result.getResults().add(r);
 
             r.setStorageId(artifactEntry.getStorageId());
-            r.setStorageId(artifactEntry.getRepositoryId());
+            r.setRepositoryId(artifactEntry.getRepositoryId());
             r.setArtifactCoordinates(artifactEntry.getArtifactCoordinates());
 
             RepositoryPath repositoryPath = artifactResolutionService.resolvePath(artifactEntry.getStorageId(),
                                                                                   artifactEntry.getRepositoryId(),
                                                                                   artifactEntry.getArtifactPath());
+
+            Repository repository = repositoryPath.getRepository();
+
             URL artifactResource = artifactResolutionService.resolveResource(repositoryPath);
             r.setUrl(artifactResource.toString());
+
+            Map<String, String> snippets = snippetGenerator.generateSnippets(repository.getLayout(),
+                                                                             artifactEntry.getArtifactCoordinates());
+            r.setSnippets(snippets);
         }
 
         return result;
