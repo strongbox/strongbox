4213c7fd4916ef15aee8b8d8120c6b6d6b5fdcce
Martin Todorov <carlspring@gmail.com>
2018-06-16 01:18:07 +0100

    Indentation fixes

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/dependency/snippet/SnippetGenerator.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/dependency/snippet/SnippetGenerator.java
@@ -9,17 +9,20 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-public class SnippetGenerator {
+public class SnippetGenerator
+{
 
     @Inject
     private CompatibleDependencyFormatRegistry compatibleDependencyFormatRegistry;
 
     public List<CodeSnippet> generateSnippets(String layout,
-                                              ArtifactCoordinates coordinates) {
+                                              ArtifactCoordinates coordinates)
+    {
         Map<String, DependencySynonymFormatter> implementations = compatibleDependencyFormatRegistry.getProviderImplementations(layout);
 
         List<CodeSnippet> snippets = new ArrayList<>();
-        for (String compatibleDependencyFormat : implementations.keySet()) {
+        for (String compatibleDependencyFormat : implementations.keySet())
+        {
             DependencySynonymFormatter formatter = implementations.get(compatibleDependencyFormat);
 
             CodeSnippet snippet = new CodeSnippet(compatibleDependencyFormat, formatter.getDependencySnippet(coordinates));
