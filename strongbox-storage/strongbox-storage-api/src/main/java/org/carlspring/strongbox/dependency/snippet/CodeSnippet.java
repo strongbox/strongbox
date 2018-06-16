00dd74d02fbdd721c692fe0836a087391289a48c
Martin Todorov <carlspring@gmail.com>
2018-06-16 01:17:04 +0100

    Indentation fixes

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/dependency/snippet/CodeSnippet.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/dependency/snippet/CodeSnippet.java
@@ -1,27 +1,37 @@
 package org.carlspring.strongbox.dependency.snippet;
 
-public class CodeSnippet {
+public class CodeSnippet
+{
+    
     protected String name;
+    
     protected String code;
-
-    public CodeSnippet(String name, String code) {
+    
+    
+    public CodeSnippet(String name, String code)
+    {
         this.name = name;
         this.code = code;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public String getCode() {
+    public String getCode()
+    {
         return code;
     }
 
-    public void setCode(String code) {
+    public void setCode(String code)
+    {
         this.code = code;
     }
+    
 }
