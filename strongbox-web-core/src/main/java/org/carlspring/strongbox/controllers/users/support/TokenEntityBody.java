2c046a88f8a6c71a20ae4ff4ca69e8028e30450f
Steve Todorov <steve.todorov@carlspring.com>
2018-07-23 15:12:59 +0300

    SB-1198 Create endpoint which returns all assignable roles

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/users/support/TokenEntityBody.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/users/support/TokenEntityBody.java
@@ -1,4 +1,4 @@
-package org.carlspring.strongbox.controllers.users;
+package org.carlspring.strongbox.controllers.users.support;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
