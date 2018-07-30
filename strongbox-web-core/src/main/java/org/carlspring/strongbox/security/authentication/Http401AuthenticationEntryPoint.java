0090bede5688b7c2ea15265894a5632e4aac00b2
sbespalov <serge.bespalov@gmail.com>
2018-07-30 16:43:06 +0700

    SB-1210: JWT authentication reworked

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/security/authentication/Http401AuthenticationEntryPoint.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/security/authentication/Http401AuthenticationEntryPoint.java
@@ -20,7 +20,7 @@ public class Http401AuthenticationEntryPoint implements AuthenticationEntryPoint
 
     private static final String IS_AJAX_REQUEST_HEADER_VALUE = "XMLHttpRequest";
 
-    private final String headerValue = "Strongbox Repository Manager";
+    private static final String STRONGBOX_REALM = "Strongbox Repository Manager";
 
     @Inject
     private ObjectMapper objectMapper;
@@ -34,7 +34,7 @@ public class Http401AuthenticationEntryPoint implements AuthenticationEntryPoint
     {        
         if (!IS_AJAX_REQUEST_HEADER_VALUE.equals(request.getHeader(IS_AJAX_REQUEST_HEADER_NAME)))
         {
-            response.setHeader("WWW-Authenticate", "Basic realm=\"" + this.headerValue + "\"");
+            response.setHeader("WWW-Authenticate", "Basic realm=\"" + STRONGBOX_REALM + "\"");
         }
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.getWriter().println(objectMapper.writeValueAsString(new ErrorResponseEntityBody("unauthorized")));
