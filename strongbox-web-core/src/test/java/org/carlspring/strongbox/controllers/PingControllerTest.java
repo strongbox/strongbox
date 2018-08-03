7e4e791fce0d3862bf115212e79e9b1d9110d644
Steve Todorov <steve.todorov@carlspring.com>
2018-08-03 23:43:30 +0300

    SB-1219 Add ANONYMOUS_USER and AUTHENTICATED_USER authorities

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/PingControllerTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/PingControllerTest.java
@@ -8,9 +8,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 /**
  * @author Steve Todorov
@@ -21,14 +23,21 @@ public class PingControllerTest
         extends MavenRestAssuredBaseTest
 {
 
+    @Override
+    public void init()
+            throws Exception
+    {
+        super.init();
+        setContextBaseUrl(getContextBaseUrl() + "/api/ping");
+    }
+
     @Test
     public void testShouldReturnPongText()
     {
-        String url = getContextBaseUrl() + "/api/ping";
 
         given().header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN_VALUE)
                .when()
-               .get(url)
+               .get(getContextBaseUrl())
                .peek()
                .then()
                .statusCode(HttpStatus.OK.value())
@@ -38,15 +47,64 @@ public class PingControllerTest
     @Test
     public void testShouldReturnPongJSON()
     {
-        String url = getContextBaseUrl() + "/api/ping";
-
         given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                .when()
-               .get(url)
+               .get(getContextBaseUrl())
                .peek()
                .then()
                .statusCode(HttpStatus.OK.value())
                .body("message", equalTo("pong"));
     }
 
+    @Test
+    public void testShouldReturnPongForAuthenticatedUsersJSON()
+    {
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/token")
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body("message", equalTo("pong"));
+    }
+
+    @Test
+    public void testShouldReturnPongForAuthenticatedUsersText()
+    {
+        given().header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/token")
+               .peek()
+               .then()
+               .statusCode(HttpStatus.OK.value())
+               .body(equalTo("pong"));
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void testAnonymousUsersShouldNotBeAbleToAccessJSON()
+    {
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/token")
+               .peek()
+               .then()
+               .log().all()
+               .statusCode(HttpStatus.UNAUTHORIZED.value())
+               .body(notNullValue());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void testAnonymousUsersShouldNotBeAbleToAccessText()
+    {
+        given().header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN_VALUE)
+               .when()
+               .get(getContextBaseUrl() + "/token")
+               .peek()
+               .then()
+               .log().all()
+               .statusCode(HttpStatus.UNAUTHORIZED.value())
+               .body(notNullValue());
+    }
 }
