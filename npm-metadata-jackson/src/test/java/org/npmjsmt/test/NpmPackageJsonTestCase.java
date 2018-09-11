778d6c98076b004b3cd36cb9c44b296fdbb2f9ae
sbespalov <serge.bespalov@gmail.com>
2018-09-11 13:51:20 +0700

    Npm Feed schema

--- npm-metadata-jackson/src/test/java/org/npmjsmt/test/NpmPackageJsonTestCase.java
+++ npm-metadata-jackson/src/test/java/org/npmjsmt/test/NpmPackageJsonTestCase.java
@@ -5,17 +5,34 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.npmjsmt.pojo.Feed;
 import org.npmjsmt.pojo.Package;
 
-public class NpmPackageJsonTestCase {
+public class NpmPackageJsonTestCase
+{
 
-	@Test
-	public void testParseTypesNode() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
+    @Test
+    public void testParseTypesNode()
+        throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
 
-		Package packageDef = mapper.readValue(getClass().getResourceAsStream("/json/types/node/package.json"),
-				Package.class);
+        Package packageDef = mapper.readValue(getClass().getResourceAsStream("/json/types/node/package.json"),
+                                              Package.class);
 
-		assertEquals("definitely-typed", packageDef.getName());
-	}
+        assertEquals("definitely-typed", packageDef.getName());
+    }
+
+    @Test
+    public void testParseAutosuggestFeed()
+        throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Feed feedDef = mapper.readValue(getClass().getResourceAsStream("/json/antlr4-autosuggest/feed.json"),
+                                        Feed.class);
+
+        assertEquals("antlr4-autosuggest", feedDef.getName());
+    }
 }
