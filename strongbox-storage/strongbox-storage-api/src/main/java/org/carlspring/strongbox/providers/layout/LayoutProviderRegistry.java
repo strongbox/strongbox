f6ea75dbd5b096e5c75afb1666d5d097188a7250
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2018-10-02 23:56:26 +0200

    SB-1192 test fixes

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/providers/layout/LayoutProviderRegistry.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/providers/layout/LayoutProviderRegistry.java
@@ -1,7 +1,7 @@
 package org.carlspring.strongbox.providers.layout;
 
-import org.carlspring.strongbox.configuration.ConfigurationManager;
 import org.carlspring.strongbox.configuration.Configuration;
+import org.carlspring.strongbox.configuration.ConfigurationManager;
 import org.carlspring.strongbox.providers.AbstractMappedProviderRegistry;
 import org.carlspring.strongbox.providers.ProviderImplementationException;
 import org.carlspring.strongbox.services.ConfigurationManagementService;
@@ -11,7 +11,7 @@ import org.carlspring.strongbox.storage.repository.Repository;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +21,8 @@ import org.springframework.stereotype.Component;
  * @author carlspring
  */
 @Component
-public class LayoutProviderRegistry extends AbstractMappedProviderRegistry<LayoutProvider>
+public class LayoutProviderRegistry
+        extends AbstractMappedProviderRegistry<LayoutProvider>
 {
 
     private static final Logger logger = LoggerFactory.getLogger(LayoutProviderRegistry.class);
@@ -33,21 +34,21 @@ public class LayoutProviderRegistry extends AbstractMappedProviderRegistry<Layou
     private ConfigurationManagementService configurationManagementService;
 
     @Inject
-    private List<LayoutProvider> layoutProviders;
-    
-    @Override
-    @PostConstruct
-    public void initialize()
-    {
-        layoutProviders.stream().forEach(lp -> addProvider(lp.getAlias(), lp));
-        logger.info("Initialized the layout provider registry.");
-    }
+    private Optional<List<LayoutProvider>> layoutProviders;
 
     public static LayoutProvider getLayoutProvider(Repository repository,
                                                    LayoutProviderRegistry layoutProviderRegistry)
             throws ProviderImplementationException
     {
         return layoutProviderRegistry.getProvider(repository.getLayout());
+    }
+
+    @Override
+    @PostConstruct
+    public void initialize()
+    {
+        layoutProviders.ifPresent(providers -> providers.stream().forEach(lp -> addProvider(lp.getAlias(), lp)));
+        logger.info("Initialized the layout provider registry.");
     }
 
     public Configuration getConfiguration()
