2d810d007bce2455cebcd2595791102a842a2fc9
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2018-12-09 12:37:13 +0100

    SB-1307 MavenProxyRepositoryProviderTestIT concurrent

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/providers/repository/MavenProxyRepositoryProviderTestIT.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/providers/repository/MavenProxyRepositoryProviderTestIT.java
@@ -5,9 +5,11 @@ import org.carlspring.strongbox.data.CacheManagerTestExecutionListener;
 import org.carlspring.strongbox.domain.ArtifactEntry;
 import org.carlspring.strongbox.providers.io.RepositoryPath;
 import org.carlspring.strongbox.providers.layout.LayoutProvider;
+import org.carlspring.strongbox.providers.layout.Maven2LayoutProvider;
 import org.carlspring.strongbox.services.ArtifactEntryService;
 import org.carlspring.strongbox.storage.Storage;
 import org.carlspring.strongbox.storage.metadata.MavenMetadataManager;
+import org.carlspring.strongbox.storage.repository.MutableRepository;
 import org.carlspring.strongbox.storage.repository.Repository;
 import org.carlspring.strongbox.testing.TestCaseWithMavenArtifactGenerationAndIndexing;
 
@@ -15,18 +17,18 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.LinkedHashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.ByteStreams;
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.artifact.repository.metadata.Metadata;
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -34,6 +36,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
 /**
  * @author carlspring
@@ -42,10 +45,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @ActiveProfiles(profiles = "test")
 @ContextConfiguration(classes = Maven2LayoutProviderCronTasksTestConfig.class)
 @TestExecutionListeners(listeners = { CacheManagerTestExecutionListener.class },
-                        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+@Execution(CONCURRENT)
 public class MavenProxyRepositoryProviderTestIT
         extends TestCaseWithMavenArtifactGenerationAndIndexing
 {
+
+    private static final String STORAGE_ID = "storage-common-proxies";
+
+    private static final String REPOSITORY_ID = "maven-central-proxies-it";
+
+    private static final String REMOTE_URL = "http://maven.ibiblio.org/maven2/";
 
     @Inject
     private ArtifactEntryService artifactEntryService;
@@ -53,43 +63,51 @@ public class MavenProxyRepositoryProviderTestIT
     @Inject
     private MavenMetadataManager mavenMetadataManager;
 
-    @BeforeEach
+    private Set<MutableRepository> getRepositories(TestInfo testInfo)
+    {
+        Set<MutableRepository> repositories = new LinkedHashSet<>();
+        repositories.add(createRepositoryMock(STORAGE_ID, getRepositoryName(REPOSITORY_ID, testInfo),
+                                              Maven2LayoutProvider.ALIAS));
+        return repositories;
+    }
+
     @AfterEach
-    public void cleanup()
+    public void removeRepositories(TestInfo testInfo)
             throws Exception
     {
-        deleteDirectoryRelativeToVaultDirectory("storages/storage-common-proxies/maven-central/org/carlspring/maven/derby-maven-plugin");
-        deleteDirectoryRelativeToVaultDirectory("storages/storage-common-proxies/maven-oracle/com/oracle/jdbc/ojdbc8");
-        deleteDirectoryRelativeToVaultDirectory("storages/storage-common-proxies/maven-central/org/carlspring/properties-injector/1.1");
-        deleteDirectoryRelativeToVaultDirectory("storages/storage-common-proxies/maven-central/javax/media/jai_core");
+        removeRepositories(getRepositories(testInfo));
+    }
+
+    @BeforeEach
+    public void init(TestInfo testInfo)
+            throws Exception
+    {
+
+        final String repositoryId = getRepositoryName(REPOSITORY_ID, testInfo);
+
+        createProxyRepository(STORAGE_ID,
+                              repositoryId,
+                              REMOTE_URL);
 
         artifactEntryService.delete(
-                artifactEntryService.findArtifactList("storage-common-proxies",
-                                                      "maven-central",
+                artifactEntryService.findArtifactList(STORAGE_ID,
+                                                      repositoryId,
                                                       ImmutableMap.of("groupId", "org.carlspring.maven",
                                                                       "artifactId", "derby-maven-plugin",
                                                                       "version", "1.10"),
                                                       true));
 
         artifactEntryService.delete(
-                artifactEntryService.findArtifactList("storage-common-proxies",
-                                                      "maven-oracle",
-                                                      ImmutableMap.of("groupId", "com.oracle.jdbc",
-                                                                      "artifactId", "ojdbc8",
-                                                                      "version", "12.2.0.1"),
-                                                      true));
-
-        artifactEntryService.delete(
-                artifactEntryService.findArtifactList("storage-common-proxies",
-                                                      "maven-central",
+                artifactEntryService.findArtifactList(STORAGE_ID,
+                                                      repositoryId,
                                                       ImmutableMap.of("groupId", "org.carlspring",
                                                                       "artifactId", "properties-injector",
                                                                       "version", "1.1"),
                                                       true));
 
         artifactEntryService.delete(
-                artifactEntryService.findArtifactList("storage-common-proxies",
-                                                      "maven-central",
+                artifactEntryService.findArtifactList(STORAGE_ID,
+                                                      repositoryId,
                                                       ImmutableMap.of("groupId", "javax.media",
                                                                       "artifactId", "jai_core",
                                                                       "version", "1.1.3"),
@@ -137,11 +155,11 @@ public class MavenProxyRepositoryProviderTestIT
     */
 
     @Test
-    public void whenDownloadingArtifactMetadaFileShouldAlsoBeResolved()
+    public void whenDownloadingArtifactMetadaFileShouldAlsoBeResolved(TestInfo testInfo)
             throws Exception
     {
-        String storageId = "storage-common-proxies";
-        String repositoryId = "maven-central";
+        String storageId = STORAGE_ID;
+        String repositoryId = getRepositoryName(REPOSITORY_ID, testInfo);
 
         assertStreamNotNull(storageId, repositoryId,
                             "org/carlspring/properties-injector/1.1/properties-injector-1.1.jar");
@@ -151,19 +169,19 @@ public class MavenProxyRepositoryProviderTestIT
 
         LayoutProvider layoutProvider = layoutProviderRegistry.getProvider(repository.getLayout());
 
-        assertTrue(layoutProvider.containsPath(repositoryPathResolver.resolve(repository, "org/carlspring/properties-injector/maven-metadata.xml")));
+        assertTrue(layoutProvider.containsPath(
+                repositoryPathResolver.resolve(repository, "org/carlspring/properties-injector/maven-metadata.xml")));
     }
 
-    @Disabled
     @Test
-    public void whenDownloadingArtifactMetadataFileShouldBeMergedWhenExist()
+    public void whenDownloadingArtifactMetadataFileShouldBeMergedWhenExist(TestInfo testInfo)
             throws Exception
     {
-        String storageId = "storage-common-proxies";
+        String storageId = STORAGE_ID;
         Storage storage = getConfiguration().getStorage(storageId);
 
         // 1. download the artifact and artifactId-level maven metadata-file from maven-central
-        String repositoryId = "maven-central";
+        String repositoryId = getRepositoryName(REPOSITORY_ID, testInfo);
         assertStreamNotNull(storageId, repositoryId, "javax/media/jai_core/1.1.3/jai_core-1.1.3-javadoc.jar");
 
         // 2. resolve downloaded artifact base path
@@ -178,7 +196,8 @@ public class MavenProxyRepositoryProviderTestIT
         FileUtils.copyDirectory(mavenCentralArtifactBaseBath.toFile(), carlspringArtifactBaseBath.toFile());
 
         // 4. confirm maven-metadata.xml lies in the carlspring repository
-        assertTrue(layoutProvider.containsPath(repositoryPathResolver.resolve(repository, "javax/media/jai_core/maven-metadata.xml")));
+        assertTrue(layoutProvider.containsPath(
+                repositoryPathResolver.resolve(repository, "javax/media/jai_core/maven-metadata.xml")));
 
         // 5. confirm some pre-merge state
         Path artifactBasePath = repositoryPathResolver.resolve(repository, "javax/media/jai_core/");
@@ -197,11 +216,11 @@ public class MavenProxyRepositoryProviderTestIT
     }
 
     @Test
-    public void whenDownloadingArtifactDatabaseShouldBeAffectedByArtifactEntry()
+    public void whenDownloadingArtifactDatabaseShouldBeAffectedByArtifactEntry(TestInfo testInfo)
             throws Exception
     {
-        String storageId = "storage-common-proxies";
-        String repositoryId = "maven-central";
+        String storageId = STORAGE_ID;
+        String repositoryId = getRepositoryName(REPOSITORY_ID, testInfo);
         String path = "org/carlspring/properties-injector/1.6/properties-injector-1.6.jar";
 
         Optional<ArtifactEntry> artifactEntry = Optional.ofNullable(artifactEntryService.findOneArtifact(storageId,
@@ -216,41 +235,44 @@ public class MavenProxyRepositoryProviderTestIT
     }
 
     @Test
-    public void testMavenCentral()
+    public void testMavenCentral(TestInfo testInfo)
             throws Exception
     {
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        String storageId = STORAGE_ID;
+        String repositoryId = getRepositoryName(REPOSITORY_ID, testInfo);
+
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/maven-metadata.xml");
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/maven-metadata.xml.md5");
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/maven-metadata.xml.sha1");
 
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/1.10/derby-maven-plugin-1.10.pom");
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/1.10/derby-maven-plugin-1.10.pom.md5");
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/1.10/derby-maven-plugin-1.10.pom.sha1");
 
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/1.10/derby-maven-plugin-1.10.jar");
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/1.10/derby-maven-plugin-1.10.jar.md5");
-        assertStreamNotNull("storage-common-proxies",
-                            "maven-central",
+        assertStreamNotNull(storageId,
+                            repositoryId,
                             "org/carlspring/maven/derby-maven-plugin/1.10/derby-maven-plugin-1.10.jar.sha1");
 
-        assertIndexContainsArtifact("storage-common-proxies",
-                                    "maven-central",
+        assertIndexContainsArtifact(storageId,
+                                    repositoryId,
                                     "+g:org.carlspring.maven +a:derby-maven-plugin +v:1.10");
     }
 
