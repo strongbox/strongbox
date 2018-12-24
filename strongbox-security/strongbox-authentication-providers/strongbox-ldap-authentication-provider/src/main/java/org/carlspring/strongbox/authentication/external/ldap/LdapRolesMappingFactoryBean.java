d632d8507d56ac91389bec17d44df1c49a4f3ac2
sbespalov <serge.bespalov@gmail.com>
2018-12-24 08:54:54 +0700

    SB-1289: CR

--- strongbox-security/strongbox-authentication-providers/strongbox-ldap-authentication-provider/src/main/java/org/carlspring/strongbox/authentication/external/ldap/LdapRolesMappingFactoryBean.java
+++ strongbox-security/strongbox-authentication-providers/strongbox-ldap-authentication-provider/src/main/java/org/carlspring/strongbox/authentication/external/ldap/LdapRolesMappingFactoryBean.java
@@ -3,7 +3,6 @@ package org.carlspring.strongbox.authentication.external.ldap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import javax.inject.Inject;
 
