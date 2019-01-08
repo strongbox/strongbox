dc749d2e517cf0216af6c3b5b7a8cd92a94c36d3
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2019-01-08 22:06:14 +0100

    SB-1232 PR improvements

--- strongbox-data-service/src/main/java/org/carlspring/strongbox/config/EmbeddedOrientDbConfig.java
+++ strongbox-data-service/src/main/java/org/carlspring/strongbox/config/EmbeddedOrientDbConfig.java
@@ -32,7 +32,7 @@ class EmbeddedOrientDbConfig
     @Value("${strongbox.server.database.path:strongbox-vault/db}")
     private String databasePath;
 
-    @Value("${strongbox.database.snapshot.resource:db-snapshot}")
+    @Value("${strongbox.database.snapshot.resource:classpath:/db-snapshot}")
     private Resource databaseSnapshotResource;
 
     @Bean(destroyMethod = "close")
