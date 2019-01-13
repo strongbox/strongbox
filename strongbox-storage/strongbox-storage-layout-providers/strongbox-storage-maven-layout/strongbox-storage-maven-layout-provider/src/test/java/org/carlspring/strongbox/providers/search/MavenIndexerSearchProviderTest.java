23d616de22bd562a7806a1420230fe97e6424ea4
carlspring <carlspring@gmail.com>
2019-01-13 22:41:45 +0000

    - Disabled the MavenIndexerSearchProviderTest. - Disabled the parallel tests for NPM.

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/providers/search/MavenIndexerSearchProviderTest.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/providers/search/MavenIndexerSearchProviderTest.java
@@ -33,6 +33,7 @@ import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 @ActiveProfiles(profiles = "test")
 @ContextConfiguration(classes = Maven2LayoutProviderTestConfig.class)
 @Execution(CONCURRENT)
+@Disabled
 public class MavenIndexerSearchProviderTest
         extends TestCaseWithMavenArtifactGenerationAndIndexing
 {
