f32a576a30bf6ad2057170319fb8d58876945a15
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2019-02-04 22:39:18 +0100

    SB-1253 testArtifactStorageAndLastVersionManagement

--- strongbox-commons/src/main/java/org/carlspring/strongbox/artifact/ArtifactTag.java
+++ strongbox-commons/src/main/java/org/carlspring/strongbox/artifact/ArtifactTag.java
@@ -3,9 +3,9 @@ package org.carlspring.strongbox.artifact;
 public interface ArtifactTag
 {
     
-    public static final String LAST_VERSION = "last-version";
-    public static final String RELEASE = "release";
+    String LAST_VERSION = "last-version";
+    String RELEASE = "release";
 
-    public String getName();
+    String getName();
     
 }
