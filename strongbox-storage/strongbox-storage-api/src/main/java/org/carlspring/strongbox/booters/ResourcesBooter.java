8d8f206a42c32bcd3d79f83dc2515beb4a10a1eb
Pablo Tirado <pablotr87@gmail.com>
2019-02-13 19:39:30 +0100

    SB-1266: Resolve bean reference issues.

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/booters/ResourcesBooter.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/booters/ResourcesBooter.java
@@ -13,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.stereotype.Component;
 
 /**
  * @author mtodorov
