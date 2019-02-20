82e3b0ffe3b9e942b2f01d000403c68f0a2af662
adavid9 <dawid.antecki7@gmail.com>
2019-02-20 20:00:32 +0100

    fix deprecation

--- strongbox-security/strongbox-authentication-providers/strongbox-default-authentication-provider/src/main/java/org/carlspring/strongbox/authentication/api/impl/xml/CacheManagerAuthenticationCache.java
+++ strongbox-security/strongbox-authentication-providers/strongbox-default-authentication-provider/src/main/java/org/carlspring/strongbox/authentication/api/impl/xml/CacheManagerAuthenticationCache.java
@@ -3,11 +3,13 @@ package org.carlspring.strongbox.authentication.api.impl.xml;
 import org.carlspring.strongbox.data.CacheName;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 public class CacheManagerAuthenticationCache implements AuthenticationCache
 {
-    private StandardPasswordEncoder standardPasswordEncoder = new StandardPasswordEncoder();
+
+    @SuppressWarnings("deprecation")
+    private org.springframework.security.crypto.password.StandardPasswordEncoder standardPasswordEncoder = new
+            org.springframework.security.crypto.password.StandardPasswordEncoder();
 
     @Cacheable(cacheNames = CacheName.User.AUTHENTICATIONS, key = "#p0", unless = "true")
     public UsernamePasswordAuthenticationToken getAuthenticationToken(String userName)
