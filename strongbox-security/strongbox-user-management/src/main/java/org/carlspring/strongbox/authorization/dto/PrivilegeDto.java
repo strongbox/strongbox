9ba05c88f29387309d08a1c79c0ac545713fbc40
Pablo Tirado <pablotr87@gmail.com>
2019-02-24 12:20:08 +0100

    Issue-965: Fix YAML and mapping for Authorization.

--- strongbox-security/strongbox-user-management/src/main/java/org/carlspring/strongbox/authorization/dto/PrivilegeDto.java
+++ strongbox-security/strongbox-user-management/src/main/java/org/carlspring/strongbox/authorization/dto/PrivilegeDto.java
@@ -1,9 +1,5 @@
 package org.carlspring.strongbox.authorization.dto;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
 import com.google.common.base.Objects;
@@ -11,13 +7,10 @@ import com.google.common.base.Objects;
 /**
  * @author mtodorov
  */
-@XmlRootElement(name = "privilege")
-@XmlAccessorType(XmlAccessType.FIELD)
 public class PrivilegeDto
         implements Serializable, PrivelegieReadContract
 {
 
-    @XmlElement(required = true)
     private String name;
 
     private String description;
