3cd59424d2eec0bdde2548e84c313f17945af129
Brian Hill <cbh@theexperienceengine.com>
2019-04-02 21:53:29 -0400

    Remove unneeded import

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/app/StrongboxSpringBootApplication.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/app/StrongboxSpringBootApplication.java
@@ -17,7 +17,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author carlspring
