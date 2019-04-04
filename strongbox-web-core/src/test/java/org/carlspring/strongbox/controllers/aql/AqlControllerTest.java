7e821dc620013c0e6ae339fb522dbbd255aeff91
Alecg956 <alechg@umich.edu>
2019-04-04 16:16:59 -0400

    disabled 2 tests

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/aql/AqlControllerTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/aql/AqlControllerTest.java
@@ -16,6 +16,7 @@ import org.carlspring.strongbox.testing.storage.repository.RepositoryManagementT
 import org.carlspring.strongbox.testing.storage.repository.TestRepository;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
 import org.springframework.http.HttpStatus;
@@ -24,6 +25,7 @@ import org.springframework.http.MediaType;
  * @author sbespalov
  *
  */
+
 @IntegrationTest
 @Execution(SAME_THREAD)
 public class AqlControllerTest extends MavenRestAssuredBaseTest
@@ -39,6 +41,7 @@ public class AqlControllerTest extends MavenRestAssuredBaseTest
 
     private static final String R1 = "sc-releases-search";
     
+    @Disabled
     @Test
     @ExtendWith({RepositoryManagementTestExecutionListener.class, ArtifactManagementTestExecutionListener.class})
     public void testSearchExcludeVersion(@TestRepository(storage = S1, repository = R1, layout = LAYOUT_NAME) Repository repository,
@@ -75,6 +78,7 @@ public class AqlControllerTest extends MavenRestAssuredBaseTest
                .body("error", Matchers.containsString("[1:103]"));
     }
 
+    @Disabled
     @Test
     @ExtendWith({RepositoryManagementTestExecutionListener.class, ArtifactManagementTestExecutionListener.class})
     public void testSearchValidMavenCoordinates(@TestRepository(storage = S1, repository = R1, layout = LAYOUT_NAME) Repository repository,
