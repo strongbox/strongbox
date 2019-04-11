f392ed6161a69b15bb798e2562adf691c3888a34
whalenda <whalenda@umich.edu>
2019-04-11 13:36:50 -0400

    Create PypiWheelDependencyFormatter.java

---
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-pypi-layout-provider/src/main/java/org/carlspring/strongbox/dependency/snippet/PypiWheelDependencyFormatter.java
@@ -0,0 +1,62 @@
+package org.carlspring.strongbox.dependency.snippet;
+
+import org.carlspring.strongbox.artifact.coordinates.ArtifactCoordinates;
+import org.carlspring.strongbox.artifact.coordinates.PypiWheelArtifactCoordinates;
+import org.carlspring.strongbox.providers.layout.AbstractLayoutProvider;
+import org.carlspring.strongbox.providers.layout.PypiLayoutProvider;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PypiWheelDependencyFormatter
+  implements DependencySynonymFormatter
+{
+  private static final Logger logger = LoggerFactory.getLogger(AbstractLayoutProvider.class);
+
+  @Inject
+  private CompatibleDependencyFormatRegistry compatibleDependencyFormatRegistry;
+
+  @PostConstruct
+  @Override
+  public void register()
+  {
+     compatibleDependencyFormatRegistry.addProviderImplementation(getLayout(), getFormatAlias(), this);
+     logger.debug("Initialized the Pypi dependency formatter.");
+  }
+
+  @Override
+  public String getLayout()
+  {
+    return PypiLayoutProvider.ALIAS;
+  }
+
+  @Override
+  public String getFormatAlias()
+  {
+    return PypiLayoutProvider.ALIAS;
+  }
+
+  @Override
+  public String getDependencySnippet(ArtifactCoordinates input_coordinates)
+  {
+    PypiWheelArtifactCoordinates coordinates = (PypiWheelArtifactCoordinates) input_coordinates;
+
+    String dist_name = coordinates.getID();
+    String sb;
+    if (dist_name.substring(dist_name.length() - 1) == "*")
+    {
+      sb = coordinates.getID() + "==" + coordinates.getVersion();
+    }
+    else
+    {
+      sb = coordinates.getID() + ">=" + coordinates.getVersion();
+    }
+
+    return sb;
+  }
+}
