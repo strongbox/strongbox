d405c3d6488f535f7eebe5807b74fecfc89f1a6c
whalenda <whalenda@umich.edu>
2019-04-12 13:01:20 -0400

    Update PypiWheelDependencyFormatterTest.java

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-pypi-layout-provider/src/test/java/org/carlspring/strongbox/dependency/snippet/PypiWheelDependencyFormatterTest.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-pypi-layout-provider/src/test/java/org/carlspring/strongbox/dependency/snippet/PypiWheelDependencyFormatterTest.java
@@ -2,6 +2,7 @@ package org.carlspring.strongbox.dependency.snippet;
 
 import org.carlspring.strongbox.artifact.coordinates.PypiWheelArtifactCoordinates;
 import org.carlspring.strongbox.dependency.snippet.PypiWheelDependencyFormatter;
+import org.carlspring.strongbox.providers.layout.PypiLayoutProvider;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,61 +14,67 @@ public class PypiWheelDependencyFormatterTest
   private ArrayList<String> formattedwheels = new ArrayList<String>();
   public PypiWheelDependencyFormatterTest()
   {
+        @Inject
+        private CompatibleDependencyFormatRegistry compatibleDependencyFormatRegistry;
+        
+        DependencySynonymFormatter formatter = compatibleDependencyFormatRegistry.getProviderImplementation(PypiLayoutProvider.ALIAS,
+                                                                                                        PypiLayoutProvider.ALIAS);
+        assertNotNull(formatter, "Failed to look up dependency synonym formatter!");
         PypiWheelArtifactCoordinates coordinates;
         // fake examples
 
         coordinates = PypiWheelArtifactCoordinates.parse("distribution-1.0-1-py27-none-any.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         coordinates = PypiWheelArtifactCoordinates.parse("example_pkg_your_username-1.5.0-py3-none-any.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         coordinates = PypiWheelArtifactCoordinates.parse("someproject-1.5.0-py2-py3-none.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         // six
         coordinates = PypiWheelArtifactCoordinates.parse("six-1.12.0-py2.py3-none-any.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         // futures
         coordinates = PypiWheelArtifactCoordinates.parse("futures-3.2.0-py2-none-any.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
         // pytz
         coordinates = PypiWheelArtifactCoordinates.parse("pytz-2018.9-3-py2.py3-none-any.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
         // numpy
         coordinates = PypiWheelArtifactCoordinates.parse("numpy-1.16.2-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         // cryptography
         coordinates = PypiWheelArtifactCoordinates.parse("cryptography-2.6.1-cp27-cp27m-macosx_10_6_intel.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         coordinates = PypiWheelArtifactCoordinates.parse("cryptography-2.6.1-0-cp27-cp27mu-manylinux1_x86_64.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         // protobuf
         coordinates = PypiWheelArtifactCoordinates.parse("protobuf-3.7.1-cp27-cp27m-macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         // virtualenv
         coordinates = PypiWheelArtifactCoordinates.parse("virtualenv-16.4.3-py2.py3-none-any.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         // coverage
         coordinates = PypiWheelArtifactCoordinates.parse("coverage-4.5.3-cp26-cp26m-macosx_10_12_x86_64.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         // docker
         coordinates = PypiWheelArtifactCoordinates.parse("docker-3.7.2-py2.py3-none-any.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         // bcrypt
         coordinates = PypiWheelArtifactCoordinates.parse("bcrypt-3.1.6-cp27-cp27m-macosx_10_6_intel.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
         coordinates = PypiWheelArtifactCoordinates.parse("bcrypt-3.1.6-cp34-cp34m-manylinux1_i686.whl");
-        wheelartifactcoordinates.add(PypiWheelDependencyFormatter.getDependencySnippet(coordinates));
+        wheelartifactcoordinates.add(formatter.getDependencySnippet(coordinates));
 
 
         // We are now populating formattedwheels with manually pre-parsed coordinate arrays
@@ -115,4 +122,3 @@ public class PypiWheelDependencyFormatterTest
 
   }
 }
-
