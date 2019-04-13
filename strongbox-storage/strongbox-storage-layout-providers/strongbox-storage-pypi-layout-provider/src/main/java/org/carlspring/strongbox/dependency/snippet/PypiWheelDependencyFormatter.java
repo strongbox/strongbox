c8eab2f4cf49a7b4bacdff137b89b477d00c93c1
whalenda <whalenda@umich.edu>
2019-04-13 14:14:21 -0400

    fixed indentation

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-pypi-layout-provider/src/main/java/org/carlspring/strongbox/dependency/snippet/PypiWheelDependencyFormatter.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-pypi-layout-provider/src/main/java/org/carlspring/strongbox/dependency/snippet/PypiWheelDependencyFormatter.java
@@ -19,47 +19,47 @@ import org.springframework.stereotype.Component;
 **/
 @Component
 public class PypiWheelDependencyFormatter
-  implements DependencySynonymFormatter
+    implements DependencySynonymFormatter
 {
-  private static final Logger logger = LoggerFactory.getLogger(AbstractLayoutProvider.class);
+    private static final Logger logger = LoggerFactory.getLogger(AbstractLayoutProvider.class);
 
-  @Inject
-  private CompatibleDependencyFormatRegistry compatibleDependencyFormatRegistry;
-  @PostConstruct
-  @Override
-  public void register()
-  {
-     compatibleDependencyFormatRegistry.addProviderImplementation(getLayout(), getFormatAlias(), this);
-     logger.debug("Initialized the Pypi dependency formatter.");
-  }
-
-  @Override
-  public String getLayout()
-  {
-    return PypiLayoutProvider.ALIAS;
-  }
-
-  @Override
-  public String getFormatAlias()
-  {
-    return PypiLayoutProvider.ALIAS;
-  }
-
-  /**
-  * This method takes in a set of Pypi Artifact Coordinates and returns the properly formatted dependency snippet
-  * @param a PyPiWheelArtifactCoordinates object
-  * @return a string representing a properly formatted dependency snippet
-  **/
-  @Override
-  public String getDependencySnippet(ArtifactCoordinates inputCoordinates)
-  {
-    PypiWheelArtifactCoordinates coordinates = (PypiWheelArtifactCoordinates) inputCoordinates;
-    String sb = coordinates.getId(); 
-
-    if (!"version".equals(coordinates.getVersion()))
+    @Inject
+    private CompatibleDependencyFormatRegistry compatibleDependencyFormatRegistry;
+    @PostConstruct
+    @Override
+    public void register()
     {
-      sb += " == " + coordinates.getVersion();
+        compatibleDependencyFormatRegistry.addProviderImplementation(getLayout(), getFormatAlias(), this);
+        logger.debug("Initialized the Pypi dependency formatter.");
     }
-    return sb;
-  }
+
+    @Override
+    public String getLayout()
+    {
+        return PypiLayoutProvider.ALIAS;
+    }
+
+    @Override
+    public String getFormatAlias()
+    {
+        return PypiLayoutProvider.ALIAS;
+    }
+
+    /**
+    * This method takes in a set of Pypi Artifact Coordinates and returns the properly formatted dependency snippet
+    * @param a PyPiWheelArtifactCoordinates object
+    * @return a string representing a properly formatted dependency snippet
+    **/
+    @Override
+    public String getDependencySnippet(ArtifactCoordinates inputCoordinates)
+    {
+        PypiWheelArtifactCoordinates coordinates = (PypiWheelArtifactCoordinates) inputCoordinates;
+        String sb = coordinates.getId(); 
+
+        if (!"version".equals(coordinates.getVersion()))
+        {
+            sb += " == " + coordinates.getVersion();
+        }
+        return sb;
+    }
 }
