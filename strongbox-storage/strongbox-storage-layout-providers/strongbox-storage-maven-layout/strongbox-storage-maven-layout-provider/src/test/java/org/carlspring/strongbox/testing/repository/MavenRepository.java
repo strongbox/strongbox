871128c64ca0ac356319701f15fc3b4d42577104
carlspring <carlspring@gmail.com>
2019-05-18 00:42:27 +0100

    Minor test repository/artifact annotation refactoring.
    * Rolled back accidentally committed code.

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/testing/repository/MavenRepository.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/testing/repository/MavenRepository.java
@@ -3,8 +3,6 @@ package org.carlspring.strongbox.testing.repository;
 import org.carlspring.strongbox.artifact.coordinates.MavenArtifactCoordinates;
 import org.carlspring.strongbox.providers.layout.Maven2LayoutProvider;
 import org.carlspring.strongbox.storage.repository.RepositoryPolicyEnum;
-import org.carlspring.strongbox.storage.repository.RepositoryStatusEnum;
-import org.carlspring.strongbox.storage.repository.RepositoryTypeEnum;
 import org.carlspring.strongbox.testing.storage.repository.RepositorySetup;
 import org.carlspring.strongbox.testing.storage.repository.TestRepository;
 
@@ -33,58 +31,16 @@ public @interface MavenRepository
     @AliasFor(annotation = TestRepository.class)
     String repositoryId() default "releases";
 
-    /**
-     * {@link RepositoryTypeEnum}
-     */
-    @AliasFor(annotation = TestRepository.class)
-    RepositoryTypeEnum type() default RepositoryTypeEnum.HOSTED;
-
-    @AliasFor(annotation = TestRepository.class)
-    String implementation() default "file-system";
-
-    @AliasFor(annotation = TestRepository.class)
-    String layout() default Maven2LayoutProvider.ALIAS;
-
-    /**
-     * {@link RepositoryPolicyEnum}
-     */
     @AliasFor(annotation = TestRepository.class)
     RepositoryPolicyEnum policy() default RepositoryPolicyEnum.RELEASE;
-
-    /**
-     * {@link RepositoryStatusEnum}
-     */
-    @AliasFor(annotation = TestRepository.class)
-    RepositoryStatusEnum status() default RepositoryStatusEnum.IN_SERVICE;
-
-    @AliasFor(annotation = TestRepository.class)
-    long artifactMaxSize() default 0;
-
-    @AliasFor(annotation = TestRepository.class)
-    boolean trashEnabled() default true;
-
-    @AliasFor(annotation = TestRepository.class)
-    boolean allowsForceDeletion() default false;
-
-    @AliasFor(annotation = TestRepository.class)
-    boolean allowsDeployment() default true;
-
-    @AliasFor(annotation = TestRepository.class)
-    boolean allowsRedeployment() default false;
-
-    @AliasFor(annotation = TestRepository.class)
-    boolean allowsDeletion() default true;
-
-    @AliasFor(annotation = TestRepository.class)
-    boolean allowsDirectoryBrowsing() default true;
-
-    @AliasFor(annotation = TestRepository.class)
-    boolean checksumHeadersEnabled() default true;
 
     @AliasFor(annotation = TestRepository.class)
     Class<? extends RepositorySetup>[] setup() default {};
 
     @AliasFor(annotation = TestRepository.class)
     boolean cleanup() default true;
+
+    @AliasFor(annotation = TestRepository.class)
+    String layout() default Maven2LayoutProvider.ALIAS;
 
 }
