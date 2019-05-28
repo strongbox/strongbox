560fbd977192af3e2a6c964fe3757a73ed71b8ec
Martin Todorov <carlspring@gmail.com>
2019-05-29 00:18:36 +0100

    Indentation fixe
    [skip ci]

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/providers/io/StorageFileSystemProvider.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/providers/io/StorageFileSystemProvider.java
@@ -192,7 +192,8 @@ public abstract class StorageFileSystemProvider
 
         RootRepositoryPath root = repositoryPath.getFileSystem().getRootDirectory();
         recursiveDeleteExceptRoot(repositoryPath, force);
-        if (!root.equals(path)) {
+        if (!root.equals(path))
+        {
             return;
         }
         
@@ -230,7 +231,8 @@ public abstract class StorageFileSystemProvider
                                                       IOException exc)
                 throws IOException
             {
-                if (root.equals(dir)) {
+                if (root.equals(dir))
+                {
                     return FileVisitResult.TERMINATE;
                 }
                 
@@ -247,6 +249,7 @@ public abstract class StorageFileSystemProvider
                                           .get();
                     throw new IOException(message, e);
                 }
+                
                 return FileVisitResult.CONTINUE;
             }
         });
