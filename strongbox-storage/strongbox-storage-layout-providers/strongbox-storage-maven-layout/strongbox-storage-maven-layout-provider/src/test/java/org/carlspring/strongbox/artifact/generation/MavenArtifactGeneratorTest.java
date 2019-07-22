676f8c47be7431cb9b798229d998f29c6ad8b47a
Pablo Tirado <pablotr87@gmail.com>
2019-07-22 20:36:24 +0200

    Issue-1165: MavenArtifactGeneratorTest migration.

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/artifact/generation/MavenArtifactGeneratorTest.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/artifact/generation/MavenArtifactGeneratorTest.java
@@ -1,17 +1,22 @@
 package org.carlspring.strongbox.artifact.generation;
 
 import org.carlspring.strongbox.config.Maven2LayoutProviderTestConfig;
+import org.carlspring.strongbox.storage.repository.Repository;
 import org.carlspring.strongbox.testing.MavenTestCaseWithArtifactGeneration;
-import org.carlspring.strongbox.testing.artifact.MavenArtifactTestUtils;
+import org.carlspring.strongbox.testing.artifact.ArtifactManagementTestExecutionListener;
+import org.carlspring.strongbox.testing.artifact.MavenTestArtifact;
+import org.carlspring.strongbox.testing.repository.MavenRepository;
+import org.carlspring.strongbox.testing.storage.repository.RepositoryManagementTestExecutionListener;
 import org.carlspring.strongbox.util.MessageDigestUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 
-import org.apache.maven.artifact.Artifact;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -22,6 +27,7 @@ import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
 /**
  * @author mtodorov
+ * @author Pablo Tirado
  */
 @SpringBootTest
 @ActiveProfiles(profiles = "test")
@@ -32,41 +38,40 @@ public class MavenArtifactGeneratorTest
 {
 
 
+    public static final String REPOSITORY_RELEASES = "matg-releases";
+
+    @ExtendWith({ RepositoryManagementTestExecutionListener.class,
+                  ArtifactManagementTestExecutionListener.class })
     @Test
-    public void testArtifactGeneration()
+    public void testArtifactGeneration(@MavenRepository(repositoryId = REPOSITORY_RELEASES)
+                                       Repository repository,
+                                       @MavenTestArtifact(repositoryId = REPOSITORY_RELEASES,
+                                                          id = "org.carlspring.strongbox.testing:matg",
+                                                          versions = "1.2.3")
+                                       Path artifactPath)
             throws Exception
     {
-        File basedir = getRepositoryBasedir(STORAGE0, "releases").getAbsoluteFile();
+        Path artifactJarPathMd5 = Paths.get(artifactPath.toString() + ".md5");
 
-        Artifact artifact = MavenArtifactTestUtils.getArtifactFromGAVTC("org.carlspring.strongbox.testing:matg:1.2.3:jar");
+        Path artifactJarPathSha1 = Paths.get(artifactPath.toString() + ".sha1");
 
-        generateArtifact(basedir.getAbsolutePath(), artifact);
+        Path artifactPomPath = Paths.get(artifactPath.toString().replaceAll("jar", "pom"));
+        Path artifactPomPathMd5 = Paths.get(artifactPath.toString().replaceAll("jar", "pom") + ".md5");
+        Path artifactPomPathSha1 = Paths.get(artifactPath.toString().replaceAll("jar", "pom") + ".sha1");
 
-        File artifactJarFile = new File(basedir, "org/carlspring/strongbox/testing/matg/1.2.3/matg-1.2.3.jar");
-        File artifactJarFileMD5 = new File(basedir,
-                                           "org/carlspring/strongbox/testing/matg/1.2.3/matg-1.2.3.jar.md5");
-        File artifactJarFileSHA1 = new File(basedir,
-                                            "org/carlspring/strongbox/testing/matg/1.2.3/matg-1.2.3.jar.sha1");
+        assertTrue(Files.exists(artifactPath), "Failed to generate JAR file!");
+        assertTrue(Files.exists(artifactJarPathMd5), "Failed to generate JAR MD5 file!");
+        assertTrue(Files.exists(artifactJarPathSha1), "Failed to generate JAR SHA1 file!");
 
-        File artifactPomFile = new File(basedir, "org/carlspring/strongbox/testing/matg/1.2.3/matg-1.2.3.pom");
-        File artifactPomFileMD5 = new File(basedir,
-                                           "org/carlspring/strongbox/testing/matg/1.2.3/matg-1.2.3.pom.md5");
-        File artifactPomFileSHA1 = new File(basedir,
-                                            "org/carlspring/strongbox/testing/matg/1.2.3/matg-1.2.3.pom.sha1");
+        assertTrue(Files.exists(artifactPomPath), "Failed to generate POM file!");
+        assertTrue(Files.exists(artifactPomPathMd5), "Failed to generate POM MD5 file!");
+        assertTrue(Files.exists(artifactPomPathSha1), "Failed to generate POM SHA1 file!");
 
-        assertTrue(artifactJarFile.exists(), "Failed to generate JAR file!");
-        assertTrue(artifactJarFileMD5.exists(), "Failed to generate JAR MD5 file!");
-        assertTrue(artifactJarFileSHA1.exists(), "Failed to generate JAR SHA1 file!");
+        String expectedJarMD5 = calculateChecksum(artifactPath, "MD5");
+        String expectedJarSHA1 = calculateChecksum(artifactPath, "SHA1");
 
-        assertTrue(artifactPomFile.exists(), "Failed to generate POM file!");
-        assertTrue(artifactPomFileMD5.exists(), "Failed to generate POM MD5 file!");
-        assertTrue(artifactPomFileSHA1.exists(), "Failed to generate POM SHA1 file!");
-
-        String expectedJarMD5 = calculateChecksum(artifactJarFile, "MD5");
-        String expectedJarSHA1 = calculateChecksum(artifactJarFile, "SHA1");
-
-        String jarMd5 = MessageDigestUtils.readChecksumFile(artifactJarFileMD5.getAbsolutePath());
-        String jarSha1 = MessageDigestUtils.readChecksumFile(artifactJarFileSHA1.getAbsolutePath());
+        String jarMd5 = MessageDigestUtils.readChecksumFile(artifactJarPathMd5.toString());
+        String jarSha1 = MessageDigestUtils.readChecksumFile(artifactJarPathSha1.toString());
 
         System.out.println("Expected  [MD5 ] (jar): " + expectedJarMD5);
         System.out.println("Generated [MD5 ] (jar): " + jarMd5);
@@ -78,11 +83,11 @@ public class MavenArtifactGeneratorTest
 
         assertEquals(expectedJarSHA1, jarSha1);
 
-        String expectedPomMD5 = calculateChecksum(artifactPomFile, "MD5");
-        String expectedPomSHA1 = calculateChecksum(artifactPomFile, "SHA1");
+        String expectedPomMD5 = calculateChecksum(artifactPomPath, "MD5");
+        String expectedPomSHA1 = calculateChecksum(artifactPomPath, "SHA1");
 
-        String pomMD5 = MessageDigestUtils.readChecksumFile(artifactPomFileMD5.getAbsolutePath());
-        String pomSHA1 = MessageDigestUtils.readChecksumFile(artifactPomFileSHA1.getAbsolutePath());
+        String pomMD5 = MessageDigestUtils.readChecksumFile(artifactPomPathMd5.toString());
+        String pomSHA1 = MessageDigestUtils.readChecksumFile(artifactPomPathSha1.toString());
 
         System.out.println("Expected  [MD5 ] (pom): " + expectedPomMD5);
         System.out.println("Generated [MD5 ] (pom): " + pomMD5);
@@ -95,21 +100,16 @@ public class MavenArtifactGeneratorTest
         assertEquals(expectedPomSHA1, pomSHA1);
     }
 
-    private String calculateChecksum(File file,
+    private String calculateChecksum(Path path,
                                      String type)
             throws Exception
     {
         byte[] buffer = new byte[4096];
         MessageDigest md = MessageDigest.getInstance(type);
 
-        DigestInputStream dis = new DigestInputStream(new FileInputStream(file), md);
-        try
+        try (DigestInputStream dis = new DigestInputStream(Files.newInputStream(path), md))
         {
             while (dis.read(buffer) != -1) ;
-        }
-        finally
-        {
-            dis.close();
         }
 
         return MessageDigestUtils.convertToHexadecimalString(md);
