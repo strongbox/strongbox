918d831e86d084b149bd3d26d8fec5171244e35a
Jose Augusto <ze12augusto@gmail.com>
2019-10-04 10:13:10 -0300

    Code improvement, change code to use MediaType.APPLICATION_JSON_VALUE instead of a string

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/security/authentication/suppliers/CustomLoginSupplierTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/security/authentication/suppliers/CustomLoginSupplierTest.java
@@ -1,15 +1,17 @@
 package org.carlspring.strongbox.security.authentication.suppliers;
 
-import org.carlspring.strongbox.config.IntegrationTest;
-import org.carlspring.strongbox.controllers.login.LoginInput;
-
 import javax.inject.Inject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.carlspring.strongbox.config.IntegrationTest;
+import org.carlspring.strongbox.controllers.login.LoginInput;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -30,7 +32,7 @@ public class CustomLoginSupplierTest
             throws Exception
     {
         MockHttpServletRequest request = new MockHttpServletRequest("post", "/api/login");
-        request.setContentType("application/json");
+        request.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
         assertThat(customLoginSupplier.supports(request)).isTrue();
     }
@@ -40,7 +42,7 @@ public class CustomLoginSupplierTest
             throws Exception
     {
         MockHttpServletRequest request = new MockHttpServletRequest("get", "/api/login");
-        request.setContentType("application/json");
+        request.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
         assertThat(customLoginSupplier.supports(request)).isFalse();
     }
