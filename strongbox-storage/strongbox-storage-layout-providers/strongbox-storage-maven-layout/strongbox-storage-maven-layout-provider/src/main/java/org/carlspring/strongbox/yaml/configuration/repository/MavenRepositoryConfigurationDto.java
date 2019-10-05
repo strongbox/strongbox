666b1b1e16700235c0b45d048ecb8814a75805ad
Brian Hill <cbrianhill@gmail.com>
2019-10-05 10:01:55 -0400

    Couple more review comments

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/main/java/org/carlspring/strongbox/yaml/configuration/repository/MavenRepositoryConfigurationDto.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/main/java/org/carlspring/strongbox/yaml/configuration/repository/MavenRepositoryConfigurationDto.java
@@ -3,6 +3,7 @@ package org.carlspring.strongbox.yaml.configuration.repository;
 import org.carlspring.strongbox.providers.layout.Maven2LayoutProvider;
 import org.carlspring.strongbox.yaml.repository.CustomRepositoryConfiguration;
 import org.carlspring.strongbox.yaml.repository.CustomRepositoryConfigurationDto;
+import org.carlspring.strongbox.storage.metadata.maven.MetadataExpirationStrategyType;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
@@ -22,7 +23,7 @@ public class MavenRepositoryConfigurationDto
     // defaults to once daily at 2 am
     private String cronExpression = "0 0 2 * * ?";
 
-    private String metadataExpirationStrategy = "checksum";
+    private String metadataExpirationStrategy = MetadataExpirationStrategyType.CHECKSUM.name();
 
     @Override
     public boolean isIndexingEnabled()
