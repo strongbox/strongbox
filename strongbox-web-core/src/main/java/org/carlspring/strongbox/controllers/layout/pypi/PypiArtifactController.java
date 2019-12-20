75a871e9afc5ea08c7ec28cbbebe8b7d58abaee8
Ankit Tomar <tomarankit.nitb@gmail.com>
2019-12-20 20:06:35 +0530

    pip install redirection end point implementaion

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/layout/pypi/PypiArtifactController.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/layout/pypi/PypiArtifactController.java
@@ -13,8 +13,12 @@ import javax.ws.rs.core.MediaType;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -25,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.google.common.collect.Sets;
 import io.swagger.annotations.ApiOperation;
@@ -47,42 +52,38 @@ public class PypiArtifactController extends BaseArtifactController
 
     private static final Set<String> VALID_FILE_TYPES = Sets.newHashSet("sdist", "bdist_wheel");
 
-    @ApiOperation(value = "This end point will be used to upload and delete a python package based on action passed in request")
+    @ApiOperation(value = "This end point will be used to upload python package.")
     @ApiResponses(value = { @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "python package was deployed successfully."),
                             @ApiResponse(code = HttpURLConnection.HTTP_INTERNAL_ERROR, message = "An error occurred while executing request."),
                             @ApiResponse(code = HttpURLConnection.HTTP_UNAVAILABLE, message = "Service Unavailable.") })
     @PreAuthorize("hasAuthority('ARTIFACTS_DEPLOY')")
     @RequestMapping(path = "/{storageId}/{repositoryId}", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA)
-    public ResponseEntity<String> uploadOrRemovePackage(
-                                                        @PathVariable(name = "storageId") String storageId,
-                                                        @PathVariable(name = "repositoryId") String repositoryId,
-                                                        @RequestPart(name = "comment", required = false) String comment,
-                                                        @RequestPart(name = "metadata_version", required = false) String metadataVersion,
-                                                        @RequestPart(name = "filetype", required = false) String filetype,
-                                                        @RequestPart(name = "protcol_version", required = false) String protcolVersion,
-                                                        @RequestPart(name = "author", required = false) String author,
-                                                        @RequestPart(name = "home_page", required = false) String homePage,
-                                                        @RequestPart(name = "download_url", required = false) String downloadUrl,
-                                                        @RequestPart(name = "platform", required = false) String platform,
-                                                        @RequestPart(name = "version", required = false) String version,
-                                                        @RequestPart(name = "description", required = false) String description,
-                                                        @RequestPart(name = "md5_digest", required = false) String md5Digest,
-                                                        @RequestPart(name = ":action", required = false) String action,
-                                                        @RequestPart(name = "name", required = false) String name,
-                                                        @RequestPart(name = "license", required = false) String license,
-                                                        @RequestPart(name = "pyversion", required = false) String pyversion,
-                                                        @RequestPart(name = "summary", required = false) String summary,
-                                                        @RequestPart(name = "author_email", required = false) String authorEmail,
-                                                        @RequestPart(name = "content", required = true) MultipartFile file,
-                                                        HttpServletRequest request)
+    public ResponseEntity<String> uploadPackage(
+                                                @PathVariable(name = "storageId") String storageId,
+                                                @PathVariable(name = "repositoryId") String repositoryId,
+                                                @RequestPart(name = "comment", required = false) String comment,
+                                                @RequestPart(name = "metadata_version", required = true) String metadataVersion,
+                                                @RequestPart(name = "filetype", required = true) String filetype,
+                                                @RequestPart(name = "protcol_version", required = false) String protcolVersion,
+                                                @RequestPart(name = "author", required = false) String author,
+                                                @RequestPart(name = "home_page", required = false) String homePage,
+                                                @RequestPart(name = "download_url", required = false) String downloadUrl,
+                                                @RequestPart(name = "platform", required = false) String platform,
+                                                @RequestPart(name = "version", required = false) String version,
+                                                @RequestPart(name = "description", required = false) String description,
+                                                @RequestPart(name = "md5_digest", required = false) String md5Digest,
+                                                @RequestPart(name = ":action", required = true) String action,
+                                                @RequestPart(name = "name", required = true) String name,
+                                                @RequestPart(name = "license", required = false) String license,
+                                                @RequestPart(name = "pyversion", required = false) String pyversion,
+                                                @RequestPart(name = "summary", required = false) String summary,
+                                                @RequestPart(name = "author_email", required = false) String authorEmail,
+                                                @RequestPart(name = "content", required = true) MultipartFile file,
+                                                HttpServletRequest request)
     {
 
-        logger.info("pip upload/uninstall request for storageId -> [{}] , repositoryId -> [{}]", storageId,
+        logger.info("python package upload request for storageId -> [{}] , repositoryId -> [{}]", storageId,
                     repositoryId);
-        logger.info("{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}-{}", comment, metadataVersion, filetype,
-                    protcolVersion, author, homePage,
-                    downloadUrl, platform, version, description, md5Digest, action, name, license, pyversion, summary,
-                    authorEmail, file.getOriginalFilename());
 
         try
         {
@@ -115,12 +116,44 @@ public class PypiArtifactController extends BaseArtifactController
         }
         catch (Exception e)
         {
-            logger.error("Failed to process pypi upload/uninstall request for storageId -> [{}] , repositoryId -> [{}]",
+            logger.error("Failed to process pypi upload request for storageId -> [{}] , repositoryId -> [{}]",
                          storageId, repositoryId, e);
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
 
+    }
+    
+    @PreAuthorize("hasAuthority('ARTIFACTS_RESOLVE')")
+    @RequestMapping(path = "{storageId}/{repositoryId}/{packageName}/", method = RequestMethod.GET)
+    public ResponseEntity<String> downloadPackage(@PathVariable(name = "storageId") String storageId,
+                                                  @PathVariable(name = "repositoryId") String repositoryId,
+                                                  @PathVariable(name = "packageName") String packageName,
+                                                  HttpServletRequest request)
+        throws Exception
+    {
+
+        logger.info("install package request for storageId -> [{}] , repositoryId -> [{}], packageName -> [{}]",
+                    storageId,
+                    repositoryId, packageName);
+
+        final Map<String, String> uriVariables = new HashMap<String, String>();
+        uriVariables.put("storageId", storageId);
+        uriVariables.put("repositoryId", repositoryId);
+        uriVariables.put("packageName", packageName);
+
+        final URI location = ServletUriComponentsBuilder
+                                                        .fromCurrentServletMapping()
+                                                        .path("{storageId}/{repositoryId}/simple/{packageName}/")
+                                                        .build()
+                                                        .expand(uriVariables)
+                                                        .toUri();
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(location);
+        return ResponseEntity.status(HttpStatus.SEE_OTHER)
+                             .headers(headers)
+                             .body(HttpStatus.SEE_OTHER.getReasonPhrase());
     }
 
     private ResponseEntity<String> validateAndUploadPackage(PypiArtifactMetadata pypiArtifactMetadata,
