f4e35ba988644c407a25dae5b7606a5035add6da
Ankit Tomar <tomarankit.nitb@gmail.com>
2019-12-24 20:59:19 +0530

    PypiArtifact Controller Test Base

---
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/layout/pypi/PypiArtifactControllerTest.java
@@ -0,0 +1,29 @@
+package org.carlspring.strongbox.controllers.layout.pypi;
+
+import org.carlspring.strongbox.rest.common.PypiRestAssuredBaseTest;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * 
+ * @author ankit.tomar
+ *
+ */
+public class PypiArtifactControllerTest extends PypiRestAssuredBaseTest
+{
+
+    @Override
+    @BeforeEach
+    public void init()
+            throws Exception
+    {
+        super.init();
+    }
+
+
+}
