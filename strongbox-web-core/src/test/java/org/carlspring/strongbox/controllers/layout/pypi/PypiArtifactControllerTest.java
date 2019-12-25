d8c1caf6638b6cdfd6c1217fa62fe27e132384ae
Ankit Tomar <tomarankit.nitb@gmail.com>
2019-12-25 12:36:05 +0530

    Unit test cases for pypi package listing

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/layout/pypi/PypiArtifactControllerTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/layout/pypi/PypiArtifactControllerTest.java
@@ -10,6 +10,7 @@ import org.carlspring.strongbox.testing.repository.PypiTestRepository;
 import org.carlspring.strongbox.testing.storage.repository.RepositoryManagementTestExecutionListener;
 
 import java.nio.file.Path;
+import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -42,8 +43,7 @@ public class PypiArtifactControllerTest extends PypiRestAssuredBaseTest
     @ExtendWith({ RepositoryManagementTestExecutionListener.class,
                   ArtifactManagementTestExecutionListener.class })
     public void testUploadPackageFlow(@PypiTestRepository(repositoryId = REPOSITORY_RELEASES, storageId = REPOSITORY_STORAGE) Repository repository,
-                                      @PypiTestArtifact(repositoryId = REPOSITORY_RELEASES, storageId = REPOSITORY_STORAGE, id = "hello_world_pipy", versions = "1.3") Path packagePath)
-        throws Exception
+                                      @PypiTestArtifact(repositoryId = REPOSITORY_RELEASES, storageId = REPOSITORY_STORAGE, id = "hello_world_pypi", versions = "1.3") Path packagePath)
     {
         final String storageId = repository.getStorage().getId();
         final String repositoryId = repository.getId();
@@ -103,7 +103,6 @@ public class PypiArtifactControllerTest extends PypiRestAssuredBaseTest
     @ExtendWith({ RepositoryManagementTestExecutionListener.class,
                   ArtifactManagementTestExecutionListener.class })
     public void testDownloadPackageRedirectionFlow(@PypiTestRepository(repositoryId = REPOSITORY_RELEASES, storageId = REPOSITORY_STORAGE) Repository repository)
-        throws Exception
     {
         final String storageId = repository.getStorage().getId();
         final String repositoryId = repository.getId();
@@ -114,7 +113,7 @@ public class PypiArtifactControllerTest extends PypiRestAssuredBaseTest
         final String expectedRedirectionUrl = getContextBaseUrl() + "/storages/" + storageId + "/" + repositoryId
                 + "/simple/" + packageName + "/";
 
-        // Upload with Invalid action
+        // Download Package redirection
         mockMvc.contentType(MediaType.APPLICATION_JSON_VALUE)
                .when()
                .get(url, storageId, repositoryId, packageName)
@@ -124,6 +123,74 @@ public class PypiArtifactControllerTest extends PypiRestAssuredBaseTest
                .statusCode(HttpStatus.SEE_OTHER.value())
                .contentType(ContentType.TEXT)
                .header(HttpHeaders.LOCATION, Matchers.is(expectedRedirectionUrl));
+
+    }
+
+    @Test
+    @ExtendWith({ RepositoryManagementTestExecutionListener.class,
+                  ArtifactManagementTestExecutionListener.class })
+    public void testDownloadPackagePathListFlow(@PypiTestRepository(repositoryId = REPOSITORY_RELEASES, storageId = REPOSITORY_STORAGE) Repository repository,
+                                                @PypiTestArtifact(repositoryId = REPOSITORY_RELEASES, storageId = REPOSITORY_STORAGE, id = "hello_world_pypi", versions = { "1.0",
+                                                                                                                                                                            "2.0",
+                                                                                                                                                                            "3.0",
+                                                                                                                                                                            "3.1",
+                                                                                                                                                                            "3.2",
+                                                                                                                                                                            "3.4",
+                                                                                                                                                                            "5.1" }) List<Path> packagePaths)
+    {
+        final String storageId = repository.getStorage().getId();
+        final String repositoryId = repository.getId();
+
+        final String packageNameNotUploaded = "helloworldpypi";
+        final String packageNameUploaded = "hello-world-pypi";
+
+        final String url = getContextBaseUrl() + "/storages/{storageId}/{repositoryId}/simple/{packageName}/";
+
+        // All packages list eligible for download :: package not uploaded
+        mockMvc.when()
+               .get(url, storageId, repositoryId, packageNameNotUploaded)
+               .then()
+               .log()
+               .all()
+               .statusCode(HttpStatus.OK.value())
+               .contentType(ContentType.HTML)
+               .body(Matchers.containsString("<title>Links for helloworldpypi</title>"),
+                     Matchers.containsString("<h1>Links for helloworldpypi</h1>\n"),
+                     Matchers.not(Matchers.containsString("<a href=")));
+
+        // Upload different version of packages generated to be used by
+        // next-case
+        packagePaths.stream().forEach(path -> {
+
+            final String uploadUrl = getContextBaseUrl() + "/storages/{storageId}/{repositoryId}";
+
+            mockMvc.contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                   .multiPart("filetype", "sdist")
+                   .multiPart(":action", "file_upload")
+                   .multiPart("name", packageNameUploaded)
+                   .multiPart("metadata_version", "1.0")
+                   .multiPart("content", path.toFile())
+                   .when()
+                   .post(uploadUrl, storageId, repositoryId)
+                   .then()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.OK.value())
+                   .contentType(ContentType.TEXT)
+                   .body(Matchers.containsString("The artifact was deployed successfully."));
+        });
+
+        // All packages list eligible for download :: package upload
+        mockMvc.when()
+               .get(url, storageId, repositoryId, packageNameUploaded)
+               .then()
+               .log()
+               .all()
+               .statusCode(HttpStatus.OK.value())
+               .contentType(ContentType.HTML)
+               .body(Matchers.containsString("<title>Links for hello-world-pypi</title>"),
+                     Matchers.containsString("<h1>Links for hello-world-pypi</h1>\n"),
+                     Matchers.containsString("<a href="));
 
     }
 
