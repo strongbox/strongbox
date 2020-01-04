b7d446708027347d40121b48585c29c5b5fa4894
Ankit Tomar <tomarankit.nitb@gmail.com>
2020-01-04 14:16:46 +0530

    pip search implementation manually tested

--- strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/layout/pypi/PypiSearchController.java
+++ strongbox-web-core/src/main/java/org/carlspring/strongbox/controllers/layout/pypi/PypiSearchController.java
@@ -15,11 +15,12 @@ import org.carlspring.strongbox.web.RepositoryMapping;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
 
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.nio.file.Path;
@@ -41,11 +42,13 @@ import org.apache.xmlrpc.common.XmlRpcStreamRequestConfig;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.collect.Lists;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -69,16 +72,15 @@ public class PypiSearchController extends BaseController
                             @ApiResponse(code = HttpURLConnection.HTTP_INTERNAL_ERROR, message = "Internal Server Error"),
                             @ApiResponse(code = HttpURLConnection.HTTP_UNAVAILABLE, message = "Repository currently not in service.") })
     @PreAuthorize("hasAuthority('ARTIFACTS_VIEW')")
-    @RequestMapping(value = { "/{storageId}/{repositoryId}/RPC2" }, method = RequestMethod.POST, produces = MediaType.APPLICATION_XML, consumes = MediaType.APPLICATION_XML)
-    public void searchPackage(@RepositoryMapping Repository repository,
-                                                HttpServletRequest request,
-                                                HttpServletResponse response)
+    @RequestMapping(value = "/{storageId}/{repositoryId}", method = RequestMethod.POST, produces = MediaType.TEXT_XML, consumes = MediaType.TEXT_XML)
+    public ResponseEntity<String> searchPackage(@RepositoryMapping Repository repository,
+                                                HttpServletRequest request)
         throws IOException,
         Exception
     {
         final String storageId = repository.getStorage().getId();
         final String repositoryId = repository.getId();
-        logger.info("Request received for search for package name [{}]", storageId, repositoryId);
+        logger.info("Request received for package search for storage [{}] , repository [{}]", storageId, repositoryId);
 
         XmlRpcDataFormat xmlRpcDataFormat = new XmlRpcDataFormat();
         xmlRpcDataFormat.setRequest(true);
@@ -92,26 +94,20 @@ public class PypiSearchController extends BaseController
         catch (Exception e)
         {
             logger.error("Something went wrong while parsing xml request..", e);
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-            response.setStatus(HttpStatus.BAD_REQUEST.value());
-            return;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
 
         String methodName = xmlRpcRequest.getMethodName();
         if (StringUtils.isEmpty(methodName) || !"search".equals(methodName))
         {
             logger.warn("Invalid Method name passed in Request.");
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Method name passed in Request.");
-            response.setStatus(HttpStatus.BAD_REQUEST.value(),"Invalid Method name passed in Request.");
-            return;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Method name passed in Request.");
         }
 
         if (xmlRpcRequest.getParameterCount() != 2)
         {
             logger.warn("Invalid params count passed in Request.");
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid params count passed in Request.");
-            response.setStatus(HttpStatus.BAD_REQUEST.value(),"Invalid params count passed in Request.");
-            return;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid params count passed in Request.");
         }
 
         Map<String, Object[]> param = (Map<String, Object[]>) xmlRpcRequest.getParameter(0);
@@ -120,72 +116,87 @@ public class PypiSearchController extends BaseController
         if (paramValues == null || paramValues.length == 0)
         {
             logger.warn("Invalid param values passed in Request.");
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid param values passed in Request.");
-            response.setStatus(HttpStatus.BAD_REQUEST.value(),"Invalid params count passed in Request.");
-            return;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid param values passed in Request.");
         }
 
         RepositoryProvider repositoryProvider = repositoryProviderRegistry.getProvider(repository.getType());
 
-        Predicate predicate = Predicate.empty();
-        predicate.and(Predicate.of(ExpOperator.EQ.of("artifactCoordinates.coordinates.packaging",
-                                                     PypiArtifactCoordinates.WHEEL_EXTENSION)));
-
-        Predicate orPredicate = Predicate.empty();
+        List<Path> searchResults = Lists.newArrayList();
         for (Object paramValue : paramValues)
         {
             String value = (String) paramValue;
-            orPredicate.or(Predicate.of(ExpOperator.LIKE.of("artifactCoordinates.coordinates.distribution",
-                                                            value)));
+
+            Paginator paginator = new Paginator();
+            Predicate predicate = Predicate.empty();
+            predicate.and(Predicate.of(ExpOperator.EQ.of("artifactCoordinates.coordinates.packaging",
+                                                         PypiArtifactCoordinates.WHEEL_EXTENSION)));
+            predicate.and(Predicate.of(ExpOperator.LIKE.of("artifactCoordinates.coordinates.distribution",
+                                                           "%" + value + "%")));
+
+            List<Path> searchResult = repositoryProvider.search(repository.getStorage().getId(), repository.getId(),
+                                                                predicate, paginator);
+
+            if (!CollectionUtils.isEmpty(searchResult))
+            {
+                searchResults.addAll(searchResult);
+            }
         }
 
-        predicate.and(orPredicate);
-
-        Paginator paginator = new Paginator();
-        List<Path> searchResult = repositoryProvider.search(repository.getStorage().getId(), repository.getId(),
-                                                            predicate, paginator);
-
-        populateXmlResponse(searchResult, response);
+        return getResponseInXmlRpcFormat(searchResults);
     }
 
-    private void populateXmlResponse(List<Path> searchResult,
-                                                  HttpServletResponse response)
+    private ResponseEntity<String> getResponseInXmlRpcFormat(List<Path> paths)
         throws IOException,
         Exception
     {
 
-        List<Object> paramsList = new ArrayList<>();
-        searchResult.stream().forEach(path -> {
+        logger.info("Found [{}] similar packages for search command.", paths.size());
+
+        List<Object[]> paramsList = new ArrayList<>();
+        Object[] paramObject = new Object[paths.size()];
+        int idx = 0;
+        for (Path path : paths)
+        {
             try
             {
                 PypiArtifactCoordinates artifactCoordinates = (PypiArtifactCoordinates) RepositoryFiles.readCoordinates((RepositoryPath) path);
 
                 Map<String, Object> params = new HashMap<>();
-                params.put("_pypi_ordering", new Integer(0));
+                params.put("_pypi_ordering", new Boolean(false));
                 params.put("version", artifactCoordinates.getVersion());
                 params.put("name", artifactCoordinates.getId());
-                params.put("summary", artifactCoordinates.getCoordinates().get("summary"));
+                params.put("summary", "summary"); // TODO :: Add summary fetched from artifact metadata
+                paramObject[idx++] = params;
 
-                paramsList.add(params);
             }
             catch (Exception e)
             {
                 logger.error("Failed to read python package path [{}]", path, e);
             }
-        });
+        }
 
-        XmlRpcRequest xmlRpcRequest = new XmlRpcRequestImpl("search", paramsList);
+        paramsList.add(paramObject);
 
-        XMLWriter xmlWriter = new CharSetXMLWriter();
-        xmlWriter.setEncoding("UTF-8");
-        xmlWriter.setIndenting(false);
-        xmlWriter.setFlushing(true);
-        xmlWriter.setWriter(new BufferedWriter(new OutputStreamWriter(response.getOutputStream(), "UTF-8")));
+        try (OutputStream outputStream = new ByteArrayOutputStream())
+        {
+            XmlRpcRequest xmlRpcRequest = new XmlRpcRequestImpl("search", paramsList);
 
-        XmlRpcStreamRequestConfig xmlRpcStreamRequestConfig = new XmlRpcHttpRequestConfigImpl();
-        TypeFactory typeFactory = new TypeFactoryImpl(null);
-        XmlRpcWriter xmlRpcWriter = new XmlRpcWriter(xmlRpcStreamRequestConfig, xmlWriter, typeFactory);
-        xmlRpcWriter.write(xmlRpcRequest);
+            XMLWriter xmlWriter = new CharSetXMLWriter();
+            xmlWriter.setEncoding("UTF-8");
+            xmlWriter.setIndenting(false);
+            xmlWriter.setFlushing(true);
+            xmlWriter.setWriter(new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8")));
+
+            XmlRpcStreamRequestConfig xmlRpcStreamRequestConfig = new XmlRpcHttpRequestConfigImpl();
+            TypeFactory typeFactory = new TypeFactoryImpl(null);
+            XmlRpcWriter xmlRpcWriter = new XmlRpcWriter(xmlRpcStreamRequestConfig, xmlWriter, typeFactory);
+            xmlRpcWriter.writeRequest(xmlRpcStreamRequestConfig, xmlRpcRequest);
+
+            byte[] output = ((ByteArrayOutputStream) outputStream).toByteArray();
+
+            return ResponseEntity.status(HttpStatus.OK)
+                                 .body(new String(output));
+        }
 
     }
 }
