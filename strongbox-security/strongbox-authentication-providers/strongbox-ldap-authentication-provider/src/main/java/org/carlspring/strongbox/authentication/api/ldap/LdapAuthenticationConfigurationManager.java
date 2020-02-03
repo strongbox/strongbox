ed7683049ad4aab988975951b86c2050e35e8722
Ankit Tomar <tomarankit.nitb@gmail.com>
2020-02-03 10:15:32 +0530

    issue-1614: Used PasswordEncoderFactories for Password encoding and matching

--- strongbox-security/strongbox-authentication-providers/strongbox-ldap-authentication-provider/src/main/java/org/carlspring/strongbox/authentication/api/ldap/LdapAuthenticationConfigurationManager.java
+++ strongbox-security/strongbox-authentication-providers/strongbox-ldap-authentication-provider/src/main/java/org/carlspring/strongbox/authentication/api/ldap/LdapAuthenticationConfigurationManager.java
@@ -13,6 +13,7 @@ import org.carlspring.strongbox.authentication.api.CustomAuthenticationItemMappe
 import org.carlspring.strongbox.authentication.support.ExternalRoleMapping;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.ldap.userdetails.LdapUserDetailsService;
 import org.springframework.stereotype.Component;
@@ -41,9 +42,6 @@ public class LdapAuthenticationConfigurationManager implements CustomAuthenticat
     @Inject
     private AuthenticationItemConfigurationManager authenticationItemConfigurationManager;
 
-    @Inject
-    private PasswordEncoder passwordEncoder;
-
     @Override
     public String getConfigurationItemId()
     {
@@ -71,7 +69,7 @@ public class LdapAuthenticationConfigurationManager implements CustomAuthenticat
             LdapUserDetailsService luds = (LdapUserDetailsService) c.getBean("ldapUserDetailsService");
 
             UserDetails user = luds.loadUserByUsername(username);
-            if (!passwordEncoder.matches(password, user.getPassword()))
+            if (!PasswordEncoderFactories.createDelegatingPasswordEncoder().matches(password, user.getPassword()))
             {
                 throw new BadCredentialsException("Credentials don't match.");
             }
