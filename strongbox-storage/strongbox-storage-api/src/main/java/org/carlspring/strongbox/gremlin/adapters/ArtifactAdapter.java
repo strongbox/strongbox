96bd0d9b240f34284e1f09dd1c610fb9a13f3102
sbespalov <serge.bespalov@gmail.com>
2020-04-02 08:03:28 +0700

    issues/1649 FetchChangesFeedCronJobTestIT fixed

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/gremlin/adapters/ArtifactAdapter.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/gremlin/adapters/ArtifactAdapter.java
@@ -12,7 +12,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
