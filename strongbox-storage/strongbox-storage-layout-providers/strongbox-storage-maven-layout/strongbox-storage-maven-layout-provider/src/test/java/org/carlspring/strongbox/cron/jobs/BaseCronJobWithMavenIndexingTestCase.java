36a3680642932ffca97305b820e4b8ffb37bbce2
sbespalov <serge.bespalov@gmail.com>
2020-04-13 11:44:26 +0700

    issues/1649 CleanupExpiredArtifactsFromProxyRepositoriesCronJobTestIT fimeout increase

--- strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/cron/jobs/BaseCronJobWithMavenIndexingTestCase.java
+++ strongbox-storage/strongbox-storage-layout-providers/strongbox-storage-maven-layout/strongbox-storage-maven-layout-provider/src/test/java/org/carlspring/strongbox/cron/jobs/BaseCronJobWithMavenIndexingTestCase.java
@@ -34,7 +34,7 @@ public class BaseCronJobWithMavenIndexingTestCase
 
     protected static final String STORAGE0 = "storage0";
 
-    protected static final long EVENT_TIMEOUT_SECONDS = 20L;
+    protected static final long EVENT_TIMEOUT_SECONDS = 30L;
 
     @Inject
     protected CronTaskConfigurationService cronTaskConfigurationService;
