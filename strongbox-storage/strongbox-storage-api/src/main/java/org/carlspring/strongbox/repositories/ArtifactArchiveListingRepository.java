9f0e6c8ac32f0d7e191b56c89fa0c458635eeb5f
Ankit Tomar <tomarankit.nitb@gmail.com>
2020-05-02 17:37:44 +0530

    Archive Listing Search Service Implementation

--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/repositories/ArtifactArchiveListingRepository.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/repositories/ArtifactArchiveListingRepository.java
@@ -1,12 +1,20 @@
 package org.carlspring.strongbox.repositories;
 
+import static org.carlspring.strongbox.db.schema.Edges.ARTIFACT_HAS_ARTIFACT_ARCHIVE_LISTING;
+import static org.carlspring.strongbox.db.schema.Vertices.ARTIFACT;
+
+import org.carlspring.strongbox.domain.Artifact;
 import org.carlspring.strongbox.domain.ArtifactArchiveListing;
 import org.carlspring.strongbox.gremlin.adapters.ArtifactArchiveListingAdapter;
+import org.carlspring.strongbox.gremlin.dsl.EntityTraversal;
 import org.carlspring.strongbox.gremlin.repositories.GremlinVertexRepository;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 
+import java.util.List;
+
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -25,6 +33,18 @@ public class ArtifactArchiveListingRepository extends GremlinVertexRepository<Ar
     protected ArtifactArchiveListingAdapter adapter()
     {
         return artifactArchiveListingAdapter;
+    }
+
+    public List<ArtifactArchiveListing> findByOutGoingEdge(Artifact artifactEntry)
+    {
+        EntityTraversal<Vertex, ArtifactArchiveListing> traversal = g().V()
+                                                                       .hasLabel(ARTIFACT)
+                                                                       .has("uuid", artifactEntry.getUuid())
+                                                                       .outE(ARTIFACT_HAS_ARTIFACT_ARCHIVE_LISTING)
+                                                                       .inV()
+                                                                       .map(artifactArchiveListingAdapter.fold());
+
+        return traversal.toList();
     }
 
 }
--- strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/services/impl/ArtifactArchiveListingSearchServiceImpl.java
+++ strongbox-storage/strongbox-storage-api/src/main/java/org/carlspring/strongbox/services/impl/ArtifactArchiveListingSearchServiceImpl.java
@@ -1,6 +1,15 @@
 package org.carlspring.strongbox.services.impl;
 
+import org.carlspring.strongbox.domain.Artifact;
+import org.carlspring.strongbox.domain.ArtifactArchiveListing;
+import org.carlspring.strongbox.repositories.ArtifactArchiveListingRepository;
 import org.carlspring.strongbox.services.ArtifactArchiveListingSearchService;
+
+import javax.inject.Inject;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,5 +21,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ArtifactArchiveListingSearchServiceImpl implements ArtifactArchiveListingSearchService
 {
+
+    @Inject
+    private ArtifactArchiveListingRepository archiveListingRepository;
+
+    @Override
+    public Set<String> fetchArchiveFileNames(Artifact artifactEntry)
+    {
+        List<ArtifactArchiveListing> artifactArchiveListings = archiveListingRepository.findByOutGoingEdge(artifactEntry);
+
+        return artifactArchiveListings.stream()
+                                      .map(ArtifactArchiveListing::getFileName)
+                                      .collect(Collectors.toSet());
+
+    }
 
 }
